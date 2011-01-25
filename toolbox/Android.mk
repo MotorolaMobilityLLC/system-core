f87d95acf4bdf1ad4f51b8804474b80110ab7eb3
Beylul Bahta <e50097@motorola.com>
2011-01-25 14:11:33 -0600

    Re-enable support for sendevent2 on JB
    This change adds back support for optimized sendevent command
    to toolbox.
    
    Ported from (CR)
    
    Change-Id: I01658995d5959577c709aa4a1e9cb17da54ac286
    Reviewed-on: http://gerrit.pcs.mot.com/462718
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Beylul Bahta <e50097@motorola.com>
    (cherry picked from commit 3582dba689969ee5c31701b7e62783f076c09ba2)
    
    Conflicts:
    	toolbox/Android.mk

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -45,6 +45,14 @@ OUR_TOOLS := \
     top \
 
 ALL_TOOLS = $(BSD_TOOLS) $(OUR_TOOLS)
+# Motorola - BEGIN - IKAPP-606 - wqnt78 - 3/10/2010 - apython enabler
+
+# Note: sendevent2 is in toolbox instead of motobox to avoid GPL issues
+
+TOOLS += sendevent2
+
+# Motorola - END - IKAPP-606 - wqnt78 - 3/10/2010 - apython enabler
+
 
 # Motorola, rknize2, 2013-Apr-16, IKJBXLINE-3829
 TOOLS += setfattr
