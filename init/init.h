5884b81689bf4a59ac22ce1c6c4ce9f79abdfb4d
Dmitry Ornatsky <cdo021c@motorola.com>
2011-10-28 15:11:26 -0500

    TCMD hits supplementary group limit
    Cherry pick (CR) increasing group count to 32
    
    Change-Id: Icd67e6ca4d81f645d2ac984ff583af25a17edfae
    Reviewed-by: Dmitry Ornatsky <cdo021c@motorola.com>
    Reviewed-by: Igor Kovalenko <Igor.Kovalenko@motorola.com>

--- init/init.h
+++ init/init.h
@@ -80,7 +80,7 @@ struct svcenvinfo {
 #define SVC_RESTART     0x100 /* Use to safely restart (stop, wait, start) a service */
 #define SVC_DISABLED_START 0x200 /* a start was requested but it was disabled at the time */
 
-#define NR_SVC_SUPP_GIDS 12    /* twelve supplementary groups */
+#define NR_SVC_SUPP_GIDS 32    /* 32 supplementary groups */
 
 #define COMMAND_RETRY_TIMEOUT 5
 
