fd25854bacee15fa1e4ae9f347610cc2cc7fbe40
Alexander Filonenko <vtp684@motorola.com>
2012-03-13 16:40:24 -0500

    init: read baseband and carrier specific init files
    Examine the androidboot.baseband= and androidboot.carrier= kernel
    command line parameters to determine the baseband and carrier and load
    additional init.rc files if present.
    
    [jcarlyle: M bringup: removed check of /proc/cpuinfo Hardware: line]
    
    Change-Id: I09ced70fcea899a5f1b65f4e60bfbf97950030ae
    Signed-off-by: Alexander Filonenko <vtp684@motorola.com>
    Signed-off-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-on: http://gerrit.mot.com/764557
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted

--- init/init.cpp
+++ init/init.cpp
@@ -1191,6 +1191,32 @@ int main(int argc, char** argv) {
 
     init_parse_config_file("/init.rc");
 
+    /* BEGIN IKKRNBSP-1013, 3/13/2012, jcarlyle, Add more init.rc layers. */
+    char path[PROP_VALUE_MAX*2], bootprop[PROP_VALUE_MAX];
+
+    /* If androidboot.baseband is set, check for a baseband-specific
+     * initialization file and read if present. */
+    if (property_get("ro.baseband", bootprop) > 0) {
+        snprintf(path, sizeof(path), "/init.%s.rc", bootprop);
+        if (access(path, R_OK) == 0) {
+            INFO("Reading baseband [%s] specific config file", bootprop);
+            init_parse_config_file(path);
+        }
+    }
+
+    /* If androidboot.carrier is set or if ro.carrier is
+     * defined in the default build properties, check for a carrier-specific
+     * initialization and read if present. */
+    if (property_get("ro.carrier", bootprop) > 0) {
+        snprintf(path, sizeof(path), "/init.%s.rc", bootprop);
+        if (access(path, R_OK) == 0) {
+            INFO("Reading bootprop [%s] specific config file", bootprop);
+            init_parse_config_file(path);
+        }
+    }
+
+    /* END IKKRNBSP-1013, 3/13/2012, jcarlyle, Add more init.rc layers. */
+
     action_for_each_trigger("early-init", action_add_queue_tail);
 
     // Queue an action that waits for coldboot done so we know ueventd has set up all of /dev...
