bc961be93eeca83507bc2ec3f94cb2da94c67ee1
Niranjan Pendharkar <npendhar@codeaurora.org>
2012-04-02 16:41:59 -0700

    Do not add default route during ifc_configure
    Avoid duplicate default routes.
    Remove adding of default routes during interface configuration as
    route add/remove operation is managed by ConnectivityService via netd.
    (cherry picked from commit 4a48635c72d91cf8decb7653948fe0c76de46098)
    
    Change-Id: I982b95a44bf91d3850ca7752e307c1b1b6a66ecf
    (cherry picked from commit ed1a80237e892a3ae6b422676941c75011fe36d0)
    
    Conflicts:
    	libnetutils/ifc_utils.c

--- libnetutils/ifc_utils.c
+++ libnetutils/ifc_utils.c
@@ -4,6 +4,7 @@
 * and/or permission notice(s).
 */
 /*
+ * Copyright (c) 2011-2013 The Linux Foundation. All rights reserved.
  * Copyright 2008, The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -722,11 +723,6 @@ ifc_configure(const char *ifname,
     }
     if (ifc_set_prefixLength(ifname, prefixLength)) {
         printerr("failed to set prefixLength %d: %s\n", prefixLength, strerror(errno));
-        ifc_close();
-        return -1;
-    }
-    if (ifc_create_default_route(ifname, gateway)) {
-        printerr("failed to set default route %s: %s\n", ipaddr_to_string(gateway), strerror(errno));
         ifc_close();
         return -1;
     }
