0907c33cbfabb26801b2a0ea01be78574cce43ce
Niranjan Pendharkar <npendhar@codeaurora.org>
2012-04-02 16:41:59 -0700

    Do not add default route during ifc_configure
    Avoid duplicate default routes.
    Remove adding of default routes during interface configuration as
    route add/remove operation is managed by ConnectivityService via netd.
    
    Change-Id: Ia5b47ed68fac71b444c51d9fa8ee7132bfb851c2
    (cherry picked from commit 4a48635c72d91cf8decb7653948fe0c76de46098)
    (cherry picked from commit 272f07a0c64b631823252ee4931c47f624b04aaa)
    (cherry picked from commit 1a696b349920c06e3fde919e6d6765c86a518787)
    (cherry picked from commit 6f6681a083267a552bebb3e52cffc7bc28701479)

--- libnetutils/ifc_utils.c
+++ libnetutils/ifc_utils.c
@@ -1,16 +1,17 @@
 /*
+ * Copyright (c) 2011-2013 The Linux Foundation. All rights reserved.
  * Copyright 2008, The Android Open Source Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
@@ -798,11 +799,6 @@ ifc_configure(const char *ifname,
     }
     if (ifc_set_prefixLength(ifname, prefixLength)) {
         printerr("failed to set prefixLength %d: %s\n", prefixLength, strerror(errno));
-        ifc_close();
-        return -1;
-    }
-    if (ifc_create_default_route(ifname, gateway)) {
-        printerr("failed to set default route %s: %s\n", ipaddr_to_string(gateway), strerror(errno));
         ifc_close();
         return -1;
     }
