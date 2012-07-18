a5920a5de88c32814573b3998b9d2b284b881684
Chris Fries <C.Fries@motorola.com>
2012-07-18 17:43:30 -0500

    charger: Reduce charger removal deKinzie time
    Charger removal deKinzie time is 10 seconds, which means your phone
    is on for 10 seconds after you remove the cable.
    
    Drop this to a half second.
    
    Change-Id: I9bc3040752bbfc58a5b5f207be98d51c85c66564
    Reviewed-on: http://gerrit.pcs.mot.com/456075
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Christopher Fries <C.Fries@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Igor Kovalenko <Igor.Kovalenko@motorola.com>
    (cherry picked from commit 55ca1e9992243b4c950bd612cab5eb4af367c058)

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -74,7 +74,7 @@ char *locale;
 
 #define BATTERY_UNKNOWN_TIME    (2 * MSEC_PER_SEC)
 #define POWER_ON_KEY_TIME       (2 * MSEC_PER_SEC)
-#define UNPLUGGED_SHUTDOWN_TIME (10 * MSEC_PER_SEC)
+#define UNPLUGGED_SHUTDOWN_TIME (MSEC_PER_SEC/2)
 
 #define LAST_KMSG_PATH          "/proc/last_kmsg"
 #define LAST_KMSG_PSTORE_PATH   "/sys/fs/pstore/console-ramoops"
