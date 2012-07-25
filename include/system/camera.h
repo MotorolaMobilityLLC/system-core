48e035b50450b771cb39784669576ca8fe74cb02
ZhangXiaodan <w4779c@motorola.com>
2012-07-25 09:44:07 +0800

    (CR) : JellyBean upmerge for MME recording framework                   Merge for env callback support
    Change-Id: I4b8d8c59c08a8111db129c73ad428656478db10d
    Change-Id: Ib68d13cf7fe94a6fb7f50d5588260a56ff8b816f
    Reviewed-on: http://gerrit.pcs.mot.com/457077
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Xin Yang <e8110c@motorola.com>
    (cherry picked from commit 60c587c23252e7148a54de84d9b2804cd7783b27)
    
    Conflicts:
    	include/system/camera.h

--- include/system/camera.h
+++ include/system/camera.h
@@ -88,6 +88,13 @@ enum {
     // Notify on autofocus start and stop. This is useful in continuous
     // autofocus - FOCUS_MODE_CONTINUOUS_VIDEO and FOCUS_MODE_CONTINUOUS_PICTURE.
     CAMERA_MSG_FOCUS_MOVE = 0x0800,       // notifyCallback
+
+    // BEGIN Motorola, w4779c, 2012-02-17, IKHSS6-8697
+    // Define Motorola-specific camera interface. It is Motorola-specific msg
+    // to pass quality related information from HAL back to Camera app.
+    CAMERA_MSG_ENV_DETECT = 0x8000,
+    // END IKHSS6-8697
+
     CAMERA_MSG_ALL_MSGS = 0xFFFF
 };
 
