ea94173e99aac1c09d224a902b0c342161c7f8bf
Lokesh Nachappa (nmpr83) <nmpr83@motorola.com>
2012-07-31 20:11:33 +0530

    Giving proper permissions to install-recovery.sh.
    This shell script patches recovery image on power up.
    
    Change-Id: Iaaf59efa6fbd5ec3c98a7f3336e9cd0b7eddd1d5
    Change-Id: I188c3d6b8f0aceebbd438cba03695b2fa5d32d59
    Reviewed-on: http://gerrit.pcs.mot.com/462571
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    (cherry picked from commit 32e531b804554ca165e1a6a2556eb1af7be537ca)

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -121,6 +121,7 @@ static const struct fs_path_config android_files[] = {
     { 00644, AID_SYSTEM,    AID_SYSTEM,    0, "data/app-private/*" },
     { 00644, AID_APP,       AID_APP,       0, "data/data/*" },
     { 00755, AID_ROOT,      AID_ROOT,      0, "init.class_main.sh" },
+    { 00500, AID_ROOT,      AID_SHELL,     0, "system/etc/install-recovery.sh" },
 
     /* the following five files are INTENTIONALLY set-uid, but they
      * are NOT included on user builds. */
