2928dfbd249c49b8cceb1d170a57704305daa097
Ravikumar Vembu <a18394@motorola.com>
2012-08-27 18:17:09 -0500

    - adbd - Exit adbd to reset the pipe on transport errors
    adbd gets into an unusable state when there are errors in the transport
    pipe. The issue is seen with automation racks where multiple devices are
    connected and there are packet errors or truncated packets from the host.
    exit adbd to reset the connection in such cases.
    
    Change-Id: Id25c2c910dafddb92708b4140c4a7c692aa44f23
    Reviewed-on: http://gerrit.pcs.mot.com/472960
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Ravikumar Vembu <a18394@motorola.com>
    (cherry picked from commit 4933bdb9372e14ae5a97e168146f2508971b9c2b)

--- adb/transport_usb.c
+++ adb/transport_usb.c
@@ -62,6 +62,7 @@ static int remote_read(apacket *p, atransport *t)
     if(check_header(p)) {
         D("remote usb: check_header failed\n");
         LOGE("remote usb: check_header failed\n");
+        exit(-1);
         return -1;
     }
 
@@ -69,6 +70,7 @@ static int remote_read(apacket *p, atransport *t)
         if(usb_read(t->usb, p->data, p->msg.data_length)){
             D("remote usb: terminated (data)\n");
             LOGE("remote usb: terminated (data)\n");
+            exit(-1);
             return -1;
         }
     }
@@ -76,6 +78,7 @@ static int remote_read(apacket *p, atransport *t)
     if(check_data(p)) {
         D("remote usb: check_data failed\n");
         LOGE("remote usb: check_data failed\n");
+        exit(-1);
         return -1;
     }
 
@@ -91,12 +94,14 @@ static int remote_write(apacket *p, atransport *t)
     if(usb_write(t->usb, &p->msg, sizeof(amessage))) {
         D("remote usb: 1 - write terminated\n");
         LOGE("remote usb: 1 - write terminated\n");
+        exit(-1);
         return -1;
     }
     if(p->msg.data_length == 0) return 0;
     if(usb_write(t->usb, &p->data, size)) {
         D("remote usb: 2 - write terminated\n");
         LOGE("remote usb: 2 - write terminated\n");
+        exit(-1);
         return -1;
     }
 
