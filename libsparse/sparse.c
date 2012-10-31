68e3fc6f86ffeebe50e839658321433526afff83
stanley.lei <stanley.lei@motorola.com>
2012-10-31 15:43:14 +0800

    Fixed bug for flashing non-4K-aligned large image
    The block size of sparse image is hardcoded as 4K in libsparse.
    When the image is not 4K-aligned, fastboot will fail to compose the
    sparse images because the last chunk will be not 4K-aligned. In this
    case, the chunk type for the last chunk is either DONT-CARE or RAW type.
    In actual usage, the partition size is always 4K-aligned, so the
    generated sparse image can be dealt as below ways:
    
    1) If the last chunk is DONT-CARE type, the chunk blocks can be simply
       rounded up to the 4k-aligned size. There is no impaction for actual
       partition.
    
    2) If the last chunk is RAW type, the last chunk will be padded with 0xFF.
       With the previous assumption for partition actual size, the flashed
       image will not exceed the actual partition size. On most cases, the
       flash will be filled 0xFF after erase operation. So there is no impaction
       here.
    
    Change-Id: I97e4205c71692fb6b8ed4234167fb2d7e61b9a8d
    Reviewed-on: http://gerrit.pcs.mot.com/493074
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Qing Chang <a21550@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/504302
    (cherry picked from commit 2527cfb98ed821bf312bff3e73f7950eb1bcc43c)

--- libsparse/sparse.c
+++ libsparse/sparse.c
@@ -132,7 +132,7 @@ static int write_all_blocks(struct sparse_file *s, struct output_file *out)
 			bb = backed_block_iter_next(bb)) {
 		if (backed_block_block(bb) > last_block) {
 			unsigned int blocks = backed_block_block(bb) - last_block;
-			write_skip_chunk(out, (int64_t)blocks * s->block_size);
+			write_skip_chunk(out, (int64_t)blocks * s->block_size, false);
 		}
 		sparse_file_write_block(out, bb);
 		last_block = backed_block_block(bb) +
@@ -142,7 +142,7 @@ static int write_all_blocks(struct sparse_file *s, struct output_file *out)
 	pad = s->len - (int64_t)last_block * s->block_size;
 	assert(pad >= 0);
 	if (pad > 0) {
-		write_skip_chunk(out, pad);
+		write_skip_chunk(out, pad, true);
 	}
 
 	return 0;
