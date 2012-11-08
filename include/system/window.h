65a5feb8e763c847f89b5185f2490f7c4f93b73a
Ramkumar Radhakrishnan <ramkumar@codeaurora.org>
2012-11-07 17:23:43 -0800

    system/core: Add command NATIVE_WINDOW_UPDATE_BUFFERS_GEOMETRY
    Add native window command to update buffer buffer width, height
    and format before queuing the buffer. This is needed to add
    support for smooth streaming
    
    Change-Id: Ia7fc608cb970078bf990f68c70d203542df5d6b2
    (cherry picked from commit 470075074f47cb34d7b871a7e6451940acc49cbe)

--- include/system/window.h
+++ include/system/window.h
@@ -268,6 +268,7 @@ enum {
     NATIVE_WINDOW_SET_BUFFERS_USER_DIMENSIONS = 15, /* private */
     NATIVE_WINDOW_SET_POST_TRANSFORM_CROP   = 16,   /* private */
     NATIVE_WINDOW_SET_BUFFERS_SIZE          = 17,   /* private */
+    NATIVE_WINDOW_UPDATE_BUFFERS_GEOMETRY   = 18,   /* private */
 };
 
 /* parameter for NATIVE_WINDOW_[API_][DIS]CONNECT */
