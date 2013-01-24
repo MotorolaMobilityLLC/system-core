a473fe3307c8bdd2f9239c1f6edd3ea383699fcc
hpeddib1 <hpeddib1@motorola.com>
2013-01-24 15:46:26 -0600

    Port support for EMU analog audio to Android 4.2
    Add new device for Motorola EMU Docks.
    
    Change-Id: I4a53557bde537e6ff16f68b33091d74235952ced
    Reviewed-on: http://gerrit.pcs.mot.com/513890
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jonathan Eklund <e11237@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: David Ding <dding@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit 72e8acc69e8cc6a726434f1b5f1bc0301fe35721)

--- include/system/audio.h
+++ include/system/audio.h
@@ -382,6 +382,7 @@ enum {
     AUDIO_DEVICE_OUT_FM                        = 0x80000,
     AUDIO_DEVICE_OUT_FM_TX                     = 0x100000,
     AUDIO_DEVICE_OUT_SPDIF                     = 0x200000,
+    AUDIO_DEVICE_OUT_EMU_DOCK                  = 0x400000,
     AUDIO_DEVICE_OUT_DEFAULT                   = AUDIO_DEVICE_BIT_DEFAULT,
     AUDIO_DEVICE_OUT_ALL      = (AUDIO_DEVICE_OUT_EARPIECE |
                                  AUDIO_DEVICE_OUT_SPEAKER |
@@ -405,6 +406,7 @@ enum {
                                  AUDIO_DEVICE_OUT_FM |
                                  AUDIO_DEVICE_OUT_FM_TX |
                                  AUDIO_DEVICE_OUT_SPDIF |
+                                 AUDIO_DEVICE_OUT_EMU_DOCK |
                                  AUDIO_DEVICE_OUT_DEFAULT),
     AUDIO_DEVICE_OUT_ALL_A2DP = (AUDIO_DEVICE_OUT_BLUETOOTH_A2DP |
                                  AUDIO_DEVICE_OUT_BLUETOOTH_A2DP_HEADPHONES |
