5b051644b8559880e7f546f0f003b30e4137da94
Vicky Sehrawat <vickys@codeaurora.org>
2013-01-29 14:35:09 -0800

    system/core: New stream type and flag for incall music delivery
    - Add new stream type and flag for incall music delivery
    
    Change-Id: I54d0d820d2dbb888d2e0cebdca63be2856eb00d8

--- include/system/audio.h
+++ include/system/audio.h
@@ -61,9 +61,10 @@ typedef enum {
     AUDIO_STREAM_TTS              = 9,  /* Transmitted Through Speaker.
                                          * Plays over speaker only, silent on other devices.
                                          */
-    AUDIO_STREAM_ACCESSIBILITY    = 10, /* For accessibility talk back prompts */
-    AUDIO_STREAM_REROUTING        = 11, /* For dynamic policy output mixes */
-    AUDIO_STREAM_PATCH            = 12, /* For internal audio flinger tracks. Fixed volume */
+    AUDIO_STREAM_INCALL_MUSIC     = 10,
+    AUDIO_STREAM_ACCESSIBILITY    = 11, /* For accessibility talk back prompts */
+    AUDIO_STREAM_REROUTING        = 12, /* For dynamic policy output mixes */
+    AUDIO_STREAM_PATCH            = 13, /* For internal audio flinger tracks. Fixed volume */
     AUDIO_STREAM_PUBLIC_CNT       = AUDIO_STREAM_TTS + 1,
     AUDIO_STREAM_CNT              = AUDIO_STREAM_PATCH + 1,
 } audio_stream_type_t;
@@ -762,9 +763,10 @@ typedef enum {
 //Qualcomm Flags
     AUDIO_OUTPUT_FLAG_LPA = 0x1000,      // use LPA
     AUDIO_OUTPUT_FLAG_TUNNEL = 0x2000,   // use Tunnel
-    AUDIO_OUTPUT_FLAG_VOIP_RX = 0x4000   // use this flag in combination with DIRECT to
+    AUDIO_OUTPUT_FLAG_VOIP_RX = 0x4000,  // use this flag in combination with DIRECT to
                                          // indicate HAL to activate EC & NS
                                          // path for VOIP calls
+    AUDIO_OUTPUT_FLAG_INCALL_MUSIC = 0x8000 //use this flag for incall music delivery
 } audio_output_flags_t;
 
 /* The audio input flags are analogous to audio output flags.
