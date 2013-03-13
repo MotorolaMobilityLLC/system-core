e7bd8998748c2991f52b1fad55fd91923d2cb4bd
Balachandar Swami <w60013@motorola.com>
2013-03-13 14:03:49 -0500

    init: rename persist.sys.usb.config on boot
    On bootup rename persist.sys.usb.config to persist.mot.usb.config. This
    will avoid on property trigger of persist.sys.usb.config which causes
    enumeration issues on bootup. The  new property will be later used in
    init usb shell script to set persist.sys.usb.config to correct value
    when bootmode is normal.
    
    Change-Id: Iaabd3d65d10d98b48d845b883d4bae8fe9e522cc
    Signed-off-by: Balachandar Swami <w60013@motorola.com>
    Change-Id: I0733d47c2a7615323c8780d02255c492f84f9e66
    Reviewed-on: http://gerrit.pcs.mot.com/524506
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <a18394@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit c8ed12ab84da1d7ffca56add00aa2f3f0b75f776)
    
    Conflicts:
    	init/property_service.cpp

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -677,6 +677,21 @@ unsigned int mt_get_chip_hw_ver(void) {
     ERROR("read hw version fail, ret: %d\n", ret);
     return 0;
 }
+/* BEGIN IKJB42MAIN-6952, 03/13/2013, w60013, rename persist.sys.usb.config */
+void update_persistent_usb_property(void)
+{
+	char currPath[PATH_MAX];
+	char newPath[PATH_MAX];
+
+	snprintf(currPath, sizeof(currPath), "%s/persist.sys.usb.config",
+				PERSISTENT_PROPERTY_DIR);
+	snprintf(newPath, sizeof(newPath), "%s/persist.mot.usb.config",
+				PERSISTENT_PROPERTY_DIR);
+	if (rename(currPath, newPath)) {
+		ERROR("Unable to rename persistent prop file %s\n", currPath);
+	}
+}
+/* END IKJB42MAIN-6952 */
 
 /* When booting an encrypted system, /data is not mounted when the
  * property service is started, so any properties stored there are
@@ -762,6 +777,7 @@ void load_system_props() {
     load_properties_from_file(PROP_PATH_SYSTEM_BUILD, NULL);
     load_properties_from_file(PROP_PATH_VENDOR_BUILD, NULL);
     load_properties_from_file(PROP_PATH_FACTORY, "ro.*");
+
     load_recovery_id_prop();
 }
 
