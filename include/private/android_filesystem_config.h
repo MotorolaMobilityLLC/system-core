afe3b7a1e4195ee7b5ad306c471e7ead88401e23
John Michelau <w14107@motorola.com>
2013-03-15 15:24:17 -0500

    Make sudo setuid-root
    Change-Id: I20fd4c578d99cd42ec99f4fc3de71d997a211bb7
    Reviewed-on: http://gerrit.pcs.mot.com/525092
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit b8e05055246c35e5caef8c7c4fb252690011180c)

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -296,6 +296,9 @@ static const struct fs_path_config android_files[] = {
     { 06755, AID_ROOT,      AID_ROOT,      0, "system/xbin/librank" },
     { 06755, AID_ROOT,      AID_ROOT,      0, "system/xbin/procrank" },
     { 06755, AID_ROOT,      AID_ROOT,      0, "system/xbin/procmem" },
+    /* the following file is INTENTIONALLY set-uid, and IS included
+     * in user builds. */
+    { 06750, AID_ROOT,      AID_DIAG,      0, "system/xbin/sudo" },
     { 04770, AID_ROOT,      AID_RADIO,     0, "system/bin/pppd-ril" },
     { 04750, AID_ROOT,      AID_SYSTEM,    0, "system/bin/perf-monitor" },
 
@@ -317,6 +320,8 @@ static const struct fs_path_config android_files[] = {
     { 00640, AID_ROOT,      AID_SHELL,     0, "fstab.*" },
     { 00400, AID_ROOT,      AID_ROOT,      0, "module_hashes" },
     { 00644, AID_ROOT,      AID_ROOT,      0, 0 },
+||||||| parent of 98280b0... IKJB42MAIN-7271 Make sudo setuid-root
+>>>>>>> 98280b0... IKJB42MAIN-7271 Make sudo setuid-root
 };
 
 static inline void fs_config(const char *path, int dir,
