bcdb4a71620a876540473ce435df4b0430b4c5c9
Chris Fries <C.Fries@motorola.com>
2013-04-09 17:41:55 -0500

    Format userdata from fs_mgr if needed
    Rather than having a init script cludge to recover userdata as
    needed, let's only recover (format) userdata when it won't mount.
    
    Minor change in init required to avoid symbol collision on make_link,
    and to include new static libraries.
    
    Change-Id: I4ba0d15c53ad6ed15bceeccd94485485b51a459c
    Reviewed-on: http://gerrit.pcs.mot.com/531080
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    SLT-Approved: Christopher Fries <qcf001@motorola.com>
    Reviewed-by: Russell Knize <rknize2@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit 79ed01c0f43132331a711b7173d7de2be441019a)

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -635,7 +635,18 @@ int fs_mgr_mount_all(struct fstab *fstab)
         } else {
             ERROR("Cannot mount filesystem on %s at %s\n",
                     fstab->recs[i].blk_device, fstab->recs[i].mount_point);
-            goto out;
+            if (!strncmp(fstab[i].mnt_point, "/data", 5)) {
+                int rc;
+                rc = recover_userdata(fstab[i].type, fstab[i].blk_dev, fstab[i].mnt_point);
+                if (!rc) {
+                    /* Userdata recovery succeeded, retry this mount. */
+                    i--;
+                    continue;
+                } else {
+                    ERROR("userdata format failed.\n");
+                }
+           }
+           goto out;
         }
     }
 
