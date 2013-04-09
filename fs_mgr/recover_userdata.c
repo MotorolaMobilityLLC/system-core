df902f4a7c5bf372725b021c939c7c5f2a41b0a2
Chris Fries <C.Fries@motorola.com>
2013-04-09 17:41:55 -0500

    Format userdata from fs_mgr if needed
    Rather than having a init script cludge to recover userdata as
    needed, let's only recover (format) userdata when it won't mount.
    
    Minor change in init required to avoid symbol collision on make_link,
    and to include new static libraries.
    
    Change-Id: I4ba0d15c53ad6ed15bceeccd94485485b51a459c
    Reviewed-on: http://gerrit.pcs.mot.com/531080
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    SLT-Approved: Christopher Fries <qcf001@motorola.com>
    Reviewed-by: Russell Knize <rknize2@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit 79ed01c0f43132331a711b7173d7de2be441019a)

--- fs_mgr/recover_userdata.c
+++ fs_mgr/recover_userdata.c
@@ -16,36 +16,32 @@
 
 /*****************************************************************************
  * Recover Userdata
- * Purpose:  Bridges a gap between GED Honeycomb devices which have a
+ * Purpose:  Bridges a gap between GED devices which have a
  *           bootloader that formats ext4 partitions, and those that do not.
  *
- *           This program tries to determine if there is a valid filesystem
- *           at the supplied block device.  If not, then it formats the block
- *           device as ext4.
- *
- * Also see:
- * http://source.android.com/tech/encryption/android_crypto_implementation.html
+ *           The current implementation simply formats the filesystem.
  *
  *****************************************************************************/
 
-#include <stdio.h>    // printf()
-#include <stdlib.h>
+#include <stdio.h>    // snprintf()
 #include <unistd.h>   // exec()
 #include <sys/types.h>// open()
 #include <sys/stat.h> // open()
 #include <fcntl.h>    // open()
+#include <sys/wait.h> // WEXITSTATUS etc.
 #include <errno.h>    // errno
 #include <cutils/partition_utils.h> // partition_wiped()
 #include <sys/mount.h>// BLKGETSIZE
 #include "make_ext4fs.h" // make_ext4fs
+#include "fs_mgr_priv.h" // ERROR
 /* Avoid redefinition warnings */
 #undef __le32
 #undef __le16
-#include <cryptfs.h>  // Crypfs footer information
+#include <cryptfs.h>
 
-#define UDLOGE printf
-#define UDLOGD printf
-#define UDLOGV printf
+#define UDLOGE ERROR
+#define UDLOGD INFO
+#define UDLOGV INFO
 
 #ifndef MS_SILENT
 #define MS_SILENT MS_VERBOSE /* MS_VERBOSE is obsolete but still used in Android mount */
@@ -53,85 +49,16 @@
 
 #define INVALID_BLOCK_SIZE -1
 
-/* is_valid_fs: Check for a validly formatted or encrypted partition
- * Return 1 for plausibly valid filesystem, 0 otherwise.
- */
-int is_valid_fs(char *fs_type, char *fs_real_blkdev, char *fs_mnt_point)
-{
-    int status;
-    off64_t off;
-    int fd;
-    unsigned int nr_sec;
-    struct crypt_mnt_ftr crypt_ftr;
-
-    /* Check the likely case first - unencrypted ext4. */
-    status = mount(fs_real_blkdev, fs_mnt_point, fs_type, MS_SILENT, 0);
-    if (status == 0) {
-        /* Valid FS! */
-        umount(fs_mnt_point);
-        return 1;
-    }
-
-    /* 2nd likely case, encrypted filesystem */
-    UDLOGD("Problem mounting the raw device, could be encrypted.\n");
-    /* Need to calculate the offset of the crypto footer and read it. */
-    if ((fd = open(fs_real_blkdev, O_RDWR)) < 0) {
-        UDLOGE("Cannot open block device.  %s\n", strerror(errno));
-        return 0;
-    }
-    if ((ioctl(fd, BLKGETSIZE, &nr_sec)) == -1) {
-        UDLOGE("Cannot get block device size.  %s\n", strerror(errno));
-        close(fd);
-        return 0;
-    }
-    off = ((off64_t)nr_sec * 512) - CRYPT_FOOTER_OFFSET;
-    if (lseek64(fd, off, SEEK_SET) == -1) {
-        UDLOGE("Cannot seek to real block device footer.  %s\n", strerror(errno));
-        close(fd);
-        return 0;
-    }
-    if ((read(fd, &crypt_ftr, sizeof(struct crypt_mnt_ftr)))
-        != sizeof(struct crypt_mnt_ftr)) {
-        UDLOGE("Cannot read real block device footer. %s\n", strerror(errno));
-        close(fd);
-        return 0;
-    }
-    close(fd);
-    if (crypt_ftr.magic == CRYPT_MNT_MAGIC && crypt_ftr.crypto_type_name[0] != '\0') {
-        UDLOGD("Success: Found footer magic and partition is encrypted with \"%s\"\n", crypt_ftr.crypto_type_name);
-        if (partition_wiped(fs_real_blkdev)) {
-            UDLOGD("Found magic but the device is wiped.\n");
-            return 0;
-        } else {
-            UDLOGD("Let vold finish decryption - likely valid encrypted FS\n");
-            if (crypt_ftr.failed_decrypt_count)
-            {
-                UDLOGE("Notice: failed decrypt count is %d\n", crypt_ftr.failed_decrypt_count);
-                /* Continue, maybe password errors. */
-            }
-            if (crypt_ftr.flags & CRYPT_ENCRYPTION_IN_PROGRESS) {
-                UDLOGE("Notice: Encryption in progress?  The filesystem will be corrupted.\n");
-                return 0;
-            }
-            return 1;
-        }
-    }
-
-    /* Least likely case: We just finished a master clear or make_ext4fs didn't finish. */
-    UDLOGD("No valid crypto footer and the device isn't mounting.\n");
-    return 0;
-}
-
-
 /* format_fs: Creates a file system.
  * Returns 0 on success, -1 on error
  */
 extern struct fs_info info;
 int format_fs(char *fs_type, char *fs_real_blkdev, char *fs_mnt_point, long int fs_blksize)
 {
-    int status;
+    int status = 0;
+    int rc = 0;
     off64_t off;
-    int fd;
+    int fd, pid;
     unsigned int nr_sec;
     struct crypt_mnt_ftr crypt_ftr;
 
@@ -144,13 +71,29 @@ int format_fs(char *fs_type, char *fs_real_blkdev, char *fs_mnt_point, long int 
         args[2] = footer_size;
         args[3] = fs_real_blkdev;
         args[4] = (char *)0;
-	if (!fork()) {
+	if (!(pid = fork())) {
 		/* This doesn't return */
 		execv("/system/bin/mkfs.f2fs_arm", args);
-		return -1;
+		exit(1);
 	}
-	wait(&status);
-        return 0;
+	for(;;) {
+		pid_t p = waitpid(pid, &status, 0);
+		if (p != pid) {
+			UDLOGE("Error waiting for child process - %d\n", p);
+			rc = -1;
+			break;
+		}
+		if (WIFEXITED(status)) {
+			rc = WEXITSTATUS(status);
+			UDLOGD("mkfs done, status %d", rc);
+			if (rc) {
+				rc = -1;
+			}
+			break;
+		}
+		UDLOGE("still waiting for mkfs.f2fs...\n");
+	}
+        return rc;
     }
     /* else, it's EXT4 */
 
@@ -194,41 +137,28 @@ int format_fs(char *fs_type, char *fs_real_blkdev, char *fs_mnt_point, long int 
     return 0;
 }
 
-int main(int argc, char* argv[])
+/* Recover userdata returns 0 on success, -1 on error. */
+
+int recover_userdata(char *fs_type, char *fs_real_blkdev, char *fs_mnt_point)
 {
-    char *fs_type;
-    char *fs_real_blkdev;
-    char *fs_mnt_point;
     long int fs_blksize = INVALID_BLOCK_SIZE;
     int status = 0;
-    if (argc < 4) {
-        printf("USAGE: %s <type> <blkdev> <mntpoint> [blksize]\n", argv[0]);
-        return -1;
-    }
 
-    fs_type = argv[1];
-    fs_real_blkdev = argv[2];
-    fs_mnt_point = argv[3];
 #ifdef BOARD_USERIMAGE_BLOCK_SIZE
     fs_blksize = BOARD_USERIMAGE_BLOCK_SIZE;
 #endif
-    if (argc > 4)
-        fs_blksize = strtol(argv[4], NULL, 10);
 
     /* Ext2/3/4 only supports these block sizes, so make sure it is sane. */
     if (fs_blksize != INVALID_BLOCK_SIZE && (fs_blksize != 1024 &&
                                              fs_blksize != 2048 &&
                                              fs_blksize != 4096))
     {
-        UDLOGE("Block size '%s' not supported; using default\n", argv[4]);
+        UDLOGE("Block size '%ld' not supported; using default\n", fs_blksize);
         fs_blksize = INVALID_BLOCK_SIZE;
     }
 
-    if (is_valid_fs(fs_type, fs_real_blkdev, fs_mnt_point)) {
-        UDLOGD("Found a valid or encrypted FS\n");
-    } else {
-        UDLOGE("Formatting %s for %s\n", fs_real_blkdev, fs_mnt_point);
-        status = format_fs(fs_type, fs_real_blkdev, fs_mnt_point, fs_blksize);
-    }
+    UDLOGE("Formatting %s for %s\n", fs_real_blkdev, fs_mnt_point);
+    status = format_fs(fs_type, fs_real_blkdev, fs_mnt_point, fs_blksize);
+
     return status;
 }
