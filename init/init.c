9f90339f60000cdfcc46494df883a9fedee0b95b
Igor Kovalenko <cik009@motorola.com>
2013-04-10 17:53:48 -0500

    init: propagate hwrev from cmdline to property space
    New bootloaders are expected to pass hw revision on command line as
    androidboot.hwrev. Put this into property for convenience.
    
    Change-Id: I79ea76c5c3b08d22b6fde6fd23bde7cec8076025
    Reviewed-on: http://gerrit.pcs.mot.com/531406
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    SLT-Approved: Christopher Fries <qcf001@motorola.com>
    (cherry picked from commit f653323fa1267435a35ef536b345aeeb00143cb4)

--- init/init.c
+++ init/init.c
@@ -817,6 +817,7 @@ static void export_kernel_boot_props(void)
         { "ro.boot.bootloader", "ro.bootloader", "unknown", },
         { "ro.boot.carrier", "ro.carrier", NULL, },
         { "ro.boot.device", "ro.hw.device", NULL, },
+        { "ro.boot.hwrev", "ro.hw.hwrev", NULL, },
         { "ro.boot.modelno", "ro.hw.modelno", NULL, },
         { "ro.boot.nav_keys", "ro.hw.nav_keys", NULL, },
         { "ro.boot.lcd_density", "ro.sf.lcd_density", NULL, },
