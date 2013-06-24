1010db189bc536f65c8fb0496f1d9e224065c77e
Xia Weizhong <w20079@motorola.com>
2013-06-24 16:38:04 +0800

    when dhcp failed, start tcpdump to monitor next dhcp session
    Change-Id: I5afc24cfb851b8a6dc9a9c59113db3f148e244ea
    Change-Id: I9cc8bedc5d6b2a7a2dbf888ae3e958615a9cc492
    Reviewed-on: http://gerrit.pcs.mot.com/547622
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Christopher Fries <c.fries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit 347d8ba545173e15a8f8d54dfd0c19d1c7222c5d)

--- libnetutils/dhcp_utils.c
+++ libnetutils/dhcp_utils.c
@@ -300,6 +300,9 @@ int dhcp_start(const char *interface)
 
     /* Wait for the daemon to return a result */
     if (wait_for_property(result_prop_name, NULL, wait_time) < 0) {
+        // BEGIN MOT W20079 IKJBDVX-772 mark it so will monitor next time
+        property_set(result_prop_name, "failed");
+        // END MOT IKJBDVX-772
         snprintf(errmsg, sizeof(errmsg), "%s", "Timed out waiting for DHCP to finish");
         return -1;
     }
@@ -511,6 +514,7 @@ int dhcp_start_renew(const char *interface)
 
     /* Wait for the daemon to return a result */
     if (wait_for_property(result_prop_name, NULL, 30) < 0) {
+        property_set(result_prop_name, "failed");//MOT W20079 IKJBDVX-772
         snprintf(errmsg, sizeof(errmsg), "%s", "Timed out waiting for DHCP Renew to finish");
         return -1;
     }
