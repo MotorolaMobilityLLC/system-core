fa296f45ab2b2cda919cd86a4d4f1540a39cfa51
Yinjun Chen <a7301c@motorola.com>
2013-07-26 09:37:43 -0500

    jb-mr2-merge-fix:fs_mgr: Update fstab members
    Due to the following commit:
    
    ab6b8522 fs_mgr: support a unified fstab format.
    
    Change-Id: I4890ddae76fb9093c0af8d8db135d3117d6591fc

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -635,9 +635,9 @@ int fs_mgr_mount_all(struct fstab *fstab)
         } else {
             ERROR("Cannot mount filesystem on %s at %s\n",
                     fstab->recs[i].blk_device, fstab->recs[i].mount_point);
-            if (!strncmp(fstab[i].mnt_point, "/data", 5)) {
+            if (!strncmp(fstab->recs[i].mount_point, "/data", 5)) {
                 int rc;
-                rc = recover_userdata(fstab[i].type, fstab[i].blk_dev, fstab[i].mnt_point);
+                rc = recover_userdata(fstab->recs[i].fs_type, fstab->recs[i].blk_device, fstab->recs[i].mount_point);
                 if (!rc) {
                     /* Userdata recovery succeeded, retry this mount. */
                     i--;
