40c69f48faa0635d9cdf73f81c932dc518fffe10
Hamad Kadmany <hkadmany@codeaurora.org>
2013-08-18 13:44:46 +0300

    system: core: Add uevent support for dvb devices
    Broadcast applications expects dvb devices to exist
    in specific location.
    
    Change-Id: I9a31183014cc370a2d948dbd1d6a36a6e89b4c13
    (cherry picked from commit c2e22e7a478db98af5f5122c14fcad07f1ae173a)

--- init/devices.cpp
+++ init/devices.cpp
@@ -851,6 +851,27 @@ static void handle_generic_device_event(struct uevent *uevent)
          base = "/dev/log/";
          make_dir(base, 0755);
          name += 4;
+     } else if (!strncmp(uevent->subsystem, "dvb", 3)) {
+         /* This imitates the file system that would be created
+          * if we were using devfs instead to preserve backward compatibility
+          * for users of dvb devices
+          */
+         int adapter_id;
+         char dev_name[20] = {0};
+
+         sscanf(name, "dvb%d.%s", &adapter_id, dev_name);
+
+         /* build dvb directory */
+         base = "/dev/dvb";
+         mkdir(base, 0755);
+
+         /* build adapter directory */
+         snprintf(devpath, sizeof(devpath), "/dev/dvb/adapter%d", adapter_id);
+         mkdir(devpath, 0755);
+
+         /* build actual device directory */
+         snprintf(devpath, sizeof(devpath), "/dev/dvb/adapter%d/%s",
+                  adapter_id, dev_name);
      } else
          base = "/dev/";
      links = get_character_device_symlinks(uevent);
