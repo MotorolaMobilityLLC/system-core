8061a7e6f0a619310b882b1e42e3a1cef1da0740
Jason Hrycay <jason.hrycay@motorola.com>
2013-09-05 13:12:27 -0500

    system/core: Add tunnel format frame size
    Add tunnel mode formats frame sizes to audio.h. This ensures frame
    size definition in one place.
    
    Change-Id: Ia01cb4cba003e538a53aaa18330ef860367859c1

--- include/system/audio.h
+++ include/system/audio.h
@@ -36,6 +36,12 @@ __BEGIN_DECLS
 /* device address used to refer to the standard remote submix */
 #define AUDIO_REMOTE_SUBMIX_DEVICE_ADDRESS "0"
 
+#define AMR_FRAMESIZE 32
+#define QCELP_FRAMESIZE 35
+#define EVRC_FRAMESIZE 23
+#define AMR_WB_FRAMESIZE 61
+#define AAC_FRAMESIZE 2048
+
 /* AudioFlinger and AudioPolicy services use I/O handles to identify audio sources and sinks */
 typedef int audio_io_handle_t;
 #define AUDIO_IO_HANDLE_NONE    0
