1f9ac654244c3a7b30c4901f93e1ae2a90be7fe5
Joel Voss <wljv10@motorola.com>
2013-09-20 18:21:58 -0500

    Instrument toolbox chmod/chown to not follow soft/hard links by default
    Ported from (CR):
    Default behavior of toolbox chmod/chown is to not follow links. This can be overridden
    by the '-L' option.
    
    Change-Id: I3c74eb70d742b0d21cfc6b9e1853243f4a4c93c5
    Signed-off-by: Joel Voss <wljv10@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/573742
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- toolbox/chown.c
+++ toolbox/chown.c
@@ -1,22 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <errno.h>
 #include <pwd.h>
 #include <grp.h>
+#include <fcntl.h>
 
 #include <unistd.h>
 #include <time.h>
+
+/* BEGIN Motorola, wljv10, IKSECURITY-322 */
+static int usage()
+{
+     fprintf(stderr, "Usage: chown [OPTION] <USER>[:GROUP] <FILE1> [FILE2] ...\n");
+     fprintf(stderr, "  -L,  Traverse links (default is to not traverse)\n");
+     return 10;
+}
+
+static int safe_chown(char *path, uid_t owner, gid_t group, int traverse_links)
+{
+    struct stat sb;
+    int ret = -1;
+    int fd;
+
+    if(traverse_links) {
+      ret = chown(path, owner, group);
+    } else {
+       if(stat(path, &sb) == 0) {
+	 if((S_ISDIR(sb.st_mode)) || (sb.st_nlink == 1)) {
+	   fd = open(path, O_RDONLY | O_NOFOLLOW);
+	   if (fd < 0) {
+	     fd = open(path, O_WRONLY | O_NOFOLLOW);
+	   }
+           if (fd >= 0) {
+	     ret = fchown(fd,owner,group);
+	     close(fd);
+	   }
+	 }
+       }
+    }
+    return(ret);
+}
+/* END IKSECURITY-322 */
 
 int chown_main(int argc, char **argv)
 {
     int i;
 
-    if (argc < 3) {
-        fprintf(stderr, "Usage: chown <USER>[:GROUP] <FILE1> [FILE2] ...\n");
-        return 10;
+    if(argc < 3) {
+/* BEGIN Motorola, wljv10, IKSECURITY-322 */
+        return usage();
+    }
+
+    int traverse_links = (strcmp(argv[1], "-L") == 0 ? 1 : 0);
+
+    if (traverse_links && argc < 4) {
+        return usage();
+    }
+
+    if (traverse_links) {
+        argc--;
+        argv++;
+/* END IKSECURITY-322 */
     }
 
     // Copy argv[1] to 'user' so we can truncate it at the period
@@ -63,10 +111,12 @@ int chown_main(int argc, char **argv)
     }
 
     for (i = 2; i < argc; i++) {
-        if (chown(argv[i], uid, gid) < 0) {
+/* BEGIN Motorola, wljv10, IKSECURITY-322 */
+      if (safe_chown(argv[i], uid, gid, traverse_links) < 0) {
+/* END IKSECURITY-322 */
             fprintf(stderr, "Unable to chown %s: %s\n", argv[i], strerror(errno));
             return 10;
-        }
+      }
     }
 
     return 0;
