062b40f8a752165aba269410493751ca0d1d9c25
Andrew Wheeler <thf876@motorola.com>
2013-09-24 14:38:44 -0500

    Support Sprint TV, ported from hss7-qc-sprint
    Changes include:
         1. Enable the write permission of media proxy properties for network setting service.
         2. Add getprop blacklist.
         3. "sprint.mm.*" property keys are changed to "media.proxy.*" due to the corresponding
            changes in MME.
    
    Change-Id: Ie437f8c82cc55e1a2857c96436ec13cd57d8eb4f
    Change-Id: I0d02605bd41eddd09c53071336e5c51661eaa9bf
    Reviewed-on: http://gerrit.pcs.mot.com/491404
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    (cherry picked from commit 4fd8b87c35a847346fd53124c7660925f6b49443)
    
    Conflicts:
    	toolbox/Android.mk
    
    Conflicts:
    	include/private/android_filesystem_config.h

--- toolbox/getprop.c
+++ toolbox/getprop.c
@@ -1,17 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 #include <cutils/properties.h>
 
 #include <sys/system_properties.h>
 #include "dynarray.h"
 
+//BEGIN MOTOROLA, kgh864, IKASANTISPRINT-1896 port IKSUNFIRE-4142
+#ifdef ENABLE_BLACKLIST
+static const char * black_list [] = {
+    "net.hostname",
+    "media.proxy.username",
+    "media.proxy.password",
+    "media.proxy.http.addr",
+    "media.proxy.rtsp.addr"
+};
+
+static bool prop_blacklisted(const char * key)
+{
+    bool ret = false;
+    unsigned int i = 0;
+    if (key == NULL)
+    {
+        return ret;
+    }
+    for (i = 0; i < sizeof(black_list)/sizeof(black_list[0]); i++)
+    {
+        if (strcmp(black_list[i], key) == 0)
+        {
+            ret = true;
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return ret;
+}
+#else
+static inline bool prop_blacklisted(const char * key)
+{
+    return false;
+}
+#endif
+//END IKASANTISPRINT-1896
+
 static void record_prop(const char* key, const char* name, void* opaque)
 {
-    strlist_t* list = opaque;
-    char temp[PROP_VALUE_MAX + PROP_NAME_MAX + 16];
-    snprintf(temp, sizeof temp, "[%s]: [%s]", key, name);
-    strlist_append_dup(list, temp);
+    //BEGIN MOTOROLA, kgh864, 06/06/2012, IKASANTISPRINT-1896
+    if (!prop_blacklisted(key)){
+        strlist_t* list = opaque;
+        char temp[PROP_VALUE_MAX + PROP_NAME_MAX + 16];
+        snprintf(temp, sizeof temp, "[%s]: [%s]", key, name);
+        strlist_append_dup(list, temp);
+    }
+    //END IKASANTISPRINT-1896
 }
 
 static void list_properties(void)
@@ -48,7 +92,12 @@ int getprop_main(int argc, char *argv[])
             default_value = "";
         }
 
-        property_get(argv[1], value, default_value);
+        //BEGIN MOTOROLA, kgh864, 06/06/2012, IKASANTISPRINT-1896 port IKSUNFIRE-4142
+        memset(value, 0, sizeof(value));
+        if (!prop_blacklisted(argv[1])) {
+            property_get(argv[1], value, default_value);
+        }
+        //END IKASANTISPRINT-1896
         printf("%s\n", value);
     }
     return 0;
