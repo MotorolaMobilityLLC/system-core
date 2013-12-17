5cbc0668404b3eed5851f3eb0bac9a6511c483a6
Robert Jones <rob.jones@dolby.com>
2013-12-17 16:32:01 +1100

    Dolby:  Add support for Dolby UDC DD/DD+ Decoder
    - System property changes to handle Dolby UDC settings
    
    Change-Id: I2d67297c6acf488b2646d5b9c4ea3f2663cc292e

--- init/property_service.c
+++ init/property_service.c
@@ -12,6 +12,25 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file was modified by Dolby Laboratories, Inc. The portions of the
+ * code that are surrounded by "DOLBY..." are copyrighted and
+ * licensed separately, as follows:
+ *
+ *  (C) 2011-2013 Dolby Laboratories, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 #include <stdio.h>
@@ -95,6 +114,13 @@ struct {
     { "persist.service.bdroid.", AID_BLUETOOTH,   0 },
     { "selinux."         , AID_SYSTEM,   0 },
     { "wc_transport.",     AID_BLUETOOTH,   AID_SYSTEM },
+#ifdef DOLBY_UDC
+    { "dolby.audio",      AID_MEDIA,    0 },
+#endif // DOLBY_END
+#ifdef DOLBY_DAP
+    // used for setting Dolby specific properties
+    { "dolby.", AID_SYSTEM,   0 },
+#endif // DOLBY_END
     { NULL, 0, 0 }
 };
 
