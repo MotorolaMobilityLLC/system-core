adbdbeafbaac6cdee558fde2e2d6745bfe316eaa
Pranav Singh <a20994@motorola.com>
2014-02-10 14:38:11 +0530

    fs: allow radio to execute init.class_main.sh
    Ideally this would be accomplished by changing the owning user
    and/or group to radio; however, mkbootfs enforces uid 0 and gid 0
    ownership on all files in the root file system.
    
    Change-Id: Id507ca25b35d7a89402c26e4d916bad649e3a194
    Reviewed-on: http://gerrit.pcs.mot.com/609453
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Christopher Fries <c.fries@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Joel Voss <jvoss@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -272,6 +272,7 @@ static const struct fs_path_config android_files[] = {
     { 00644, AID_SYSTEM,    AID_SYSTEM,    0, "data/app-private/*" },
     { 00644, AID_APP,       AID_APP,       0, "data/data/*" },
     { 00755, AID_ROOT,      AID_ROOT,      0, "system/bin/ping" },
+    { 00755, AID_ROOT,      AID_ROOT,      0, "init.class_main.sh" },
 
     /* the following file is INTENTIONALLY set-gid and not set-uid.
      * Do not change. */
