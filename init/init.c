99c2855937c468cef998331a79d8f7aa8d0ca410
Joel Voss <jvoss@motorola.com>
2014-03-03 12:08:16 -0600

    Set SELinux to global permissive mode (mkkmr1-d-merge-hack)
    Added a default boot prop to set SELinux to permissive mode until
    policy is developed/integrated for 8084.
    
    Change-Id: I0e7e1842459f6f879c144a641f93370d412365ca
    Signed-off-by: Joel Voss <jvoss@motorola.com>

--- init/init.c
+++ init/init.c
@@ -731,6 +731,7 @@ static void export_kernel_boot_props(void)
         { "ro.boot.mode", "ro.bootmode", "unknown", },
         { "ro.boot.baseband", "ro.baseband", "unknown", },
         { "ro.boot.bootloader", "ro.bootloader", "unknown", },
+        { "ro.boot.selinux", "ro.boot.selinux", "permissive", },
     };
 
     for (i = 0; i < ARRAY_SIZE(prop_map); i++) {
