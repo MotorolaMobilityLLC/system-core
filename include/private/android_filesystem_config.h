bbcbe681866c768906a4e952589b604aee942603
Louis Begin <w15621@motorola.com>
2014-03-07 11:58:06 -0600

    IKFPS-216 FEATURE-3998: Add permission group fingerp
    RCA: improve security for accessing the fingerprint
    SPI driver.
    
    SOLUTION: Add a new permission group "fingerp" in
    android_filesystem_config.h.
    
    Use it in the other commits under the same CR IKFPS-98
    for running the driver and all of its assoiated file
    to that group. Also map a Signature protected permission
    to this group in MotoSignatureApp which application can
    use for accessing the driver.
    
    Change-Id: I128435683d9151e78eaaa73e77408c85da1d55c3
    Reviewed-on: http://gerrit.pcs.mot.com/617070
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <c.fries@motorola.com>
    SLTApproved: Christopher Fries <c.fries@motorola.com>
    Reviewed-by: Joel Voss <jvoss@motorola.com>
    Reviewed-by: Neil Thomas <nthomas@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/624449
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Louis Begin <begin@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -154,6 +154,7 @@
 #define AID_MOT_CAIF      9010  /* can create CAIF sockets */
 #define AID_MOT_DLNA      9011  /*DLNA native */
 #define AID_MOT_ATVC      9012  /* mot_atvc - This is for use of the ATVC service ONLY */
+#define AID_FINGERP       9015  /* IKFPS-98 Add permission group for fingerprint */
 #define AID_MOT_ESDFS     9016  /* mot_esdfs for ESDFS package list parsing */
 #define AID_ITSON         9018  /* itson permission  */
 #define AID_MOT_DTV       9019 /* dtv */
@@ -292,6 +293,7 @@ static const struct android_id_info android_ids[] = {
     { "mot_dlna",	AID_MOT_DLNA, },
     { "mot_atvc",      AID_MOT_ATVC, },
     { "mot_dbvc",      AID_MOT_DBVC, },
+    { "fingerp",        AID_FINGERP, },
     { "mot_esdfs",     AID_MOT_ESDFS, },
     { "itson",         AID_ITSON, },
     { "mot_dtv",       AID_MOT_DTV, },
