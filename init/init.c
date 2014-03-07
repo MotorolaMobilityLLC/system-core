1964c40c334b10c7bf985cef6be79e79ab98f937
Konstantin Makariev <hcv867@motorola.com>
2014-03-07 14:54:03 -0600

    init: export hwrev and device name to property space
    Export androidboot.hwrev and androidboot.device into property
    space for convenience. These properties are used by automated
    touch frimware upgrade script to pick correct firmware file.
    
    Change-Id: I884ddcb55dbb2daf976ea1ca1d6a45e9881d78ab
    Signed-off-by: Konstantin Makariev <hcv867@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/616262
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Himanshu Shah <himanshushah@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- init/init.c
+++ init/init.c
@@ -732,6 +732,8 @@ static void export_kernel_boot_props(void)
         { "ro.boot.baseband", "ro.baseband", "unknown", },
         { "ro.boot.bootloader", "ro.bootloader", "unknown", },
         { "ro.boot.selinux", "ro.boot.selinux", "permissive", },
+        { "ro.boot.device", "ro.hw.device", NULL, },
+        { "ro.boot.hwrev", "ro.hw.hwrev", NULL, },
     };
 
     for (i = 0; i < ARRAY_SIZE(prop_map); i++) {
