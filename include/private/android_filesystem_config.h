6d16602c39d30f78fca0435b0f473c3f0a6a74d8
Russ W. Knize <Russ.Knize@motorola.com>
2014-05-29 15:43:06 -0500

    esdfs: add new AID for package list parser
    Change-Id: Iaeb6f322cbed324188072b0f5450d938209abe7d
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/640401
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    SLTApproved: Connie Zhao <czhao1@motorola.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -117,6 +117,7 @@
 #define AID_MOT_ESDFS     9016  /* mot_esdfs for ESDFS package list parsing */
 #define AID_SPRINT_EXTENSION 9013  /* IKASANTISPRINT-149 sprint extension service */
 #define AID_MOT_DBVC      9014  /* mot_dbvc - This group is used to access DataBlock feature related data */
+#define AID_MOT_ESDFS     9016  /* mot_esdfs for ESDFS package list parsing */
 
 #define AID_MOT_DBVC      9014  /* mot_dbvc - This group is used to access DataBlock feature */
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
@@ -225,6 +226,7 @@ static const struct android_id_info android_ids[] = {
     { "mot_dlna",	AID_MOT_DLNA, },
     { "sprint_extension", AID_SPRINT_EXTENSION, },
     { "mot_dbvc",      AID_MOT_DBVC, },
+    { "mot_esdfs",     AID_MOT_ESDFS, },
     { "mot_esdfs",     AID_MOT_ESDFS, },
 };
 
