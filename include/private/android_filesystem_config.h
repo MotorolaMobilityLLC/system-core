aad42797f67a084e4b30b2b203f19c0ba095992e
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2014-06-24 12:09:27 -0700

    system: core: Add Sensors group
    Defining group for sensors.
    
    CRs-Fixed: 431081
    
    Change-Id: I58151f2fd727bded1b16adb3caeac85e27e44036

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -116,6 +116,8 @@
 #define AID_QCOM_DIAG 3009 /* can read/write /dev/diag */
 #define AID_IMS 3010 /* can read/write /dev/socket/imsrtp */
 
+#define AID_SENSORS       3011 /* access to /dev/socket/sensor_ctl_socket & QCCI/QCSI */
+
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
 
@@ -191,6 +193,7 @@ static const struct android_id_info android_ids[] = {
     { "net_bt_stack",  AID_NET_BT_STACK, },
     { "qcom_diag", AID_QCOM_DIAG, },
     { "ims", AID_IMS, },
+    { "sensors",       AID_SENSORS, },
 
     { "misc",          AID_MISC, },
     { "mot_accy",	AID_MOT_ACCY, },
