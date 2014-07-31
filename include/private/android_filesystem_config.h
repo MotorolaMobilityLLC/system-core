d29338c4e36434f80fc2f607980ccdefe628c490
Lianwei Wang <a22439@motorola.com>
2014-07-31 13:24:20 -0700

    PORT (CR) power: add power user and group
    Add the new power user and group to system so that the Power
    Management related files can be handled by power user and group.
    
    Change-Id: I21fe705dfe9f04709451f7ffc6b0f1854129e930
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Reviewed-on: http://gerrit.mot.com/659181
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Vanhoof <jvanhoof@motorola.com>
    Reviewed-by: Ravi Chebolu <arc095@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Reviewed-by: Ke Lu <a2435c@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>
    Reviewed-on: http://gerrit.mot.com/663181
    Reviewed-by: Lian-Wei Wang <lian-wei.wang@motorola.com>
    SLTApproved: Tomasz Jamrozy <tjamrozy@motorola.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -115,6 +115,7 @@
 #define AID_SPRINT_EXTENSION 9013  /* IKASANTISPRINT-149 sprint extension service */
 #define AID_MOT_DBVC      9014  /* mot_dbvc - This group is used to access DataBlock feature related data */
 #define AID_MOT_ESDFS     9016  /* mot_esdfs for ESDFS package list parsing */
+#define AID_POWER         9017 /* power management */
 
 #define AID_MOT_DBVC      9014  /* mot_dbvc - This group is used to access DataBlock feature */
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
@@ -218,6 +219,7 @@ static const struct android_id_info android_ids[] = {
     { "mot_esdfs",     AID_MOT_ESDFS, },
     { "mot_atvc",      AID_MOT_ATVC, },
     { "mot_esdfs",     AID_MOT_ESDFS, },
+    { "power",         AID_POWER, },
 };
 
 #define android_id_count \
