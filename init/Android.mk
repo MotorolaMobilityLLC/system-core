22f057d0ad0ee70840c44793b4e65f00121c792d
Dmitry Sokolov <dima@motorola.com>
2014-08-27 10:56:57 -0700

    Allow disabling SELinux for not signed user builds
    Change-Id: Ic7e7cd36c358a983908b97c0749f13c0b75ad0a6
    Reviewed-on: http://gerrit.mot.com/666576
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Submit-Approved: Dmitry Sokolov <dima@motorola.com>
    Tested-by: Dmitry Sokolov <dima@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>

--- init/Android.mk
+++ init/Android.mk
@@ -7,7 +7,12 @@ LOCAL_PATH:= $(call my-dir)
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 init_options += -DALLOW_LOCAL_PROP_OVERRIDE=1 -DALLOW_PERMISSIVE_SELINUX=1
 else
-init_options += -DALLOW_LOCAL_PROP_OVERRIDE=0 -DALLOW_PERMISSIVE_SELINUX=0
+init_options += -DALLOW_LOCAL_PROP_OVERRIDE=0
+ifeq ($(RADIO_SECURE),1)
+init_options += -DALLOW_PERMISSIVE_SELINUX=0
+else
+init_options += -DALLOW_PERMISSIVE_SELINUX=1
+endif
 endif
 
 init_options += -DLOG_UEVENTS=0
