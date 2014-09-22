695e9856781018c91c498c457c33580d533969e5
Chris Fries <cfries@motorola.com>
2014-09-22 14:35:54 -0400

    init: Handle block device uevent before platform uevent
    In case the bootdevice uevent shows up out of order, still remember
    how to form its links.
    
    Change-Id: Ib5b4a3bc12bba1f215d75cff2b28c5d02ba3fb04
    Reviewed-on: http://gerrit.mot.com/756070
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>
    Reviewed-by: Joel Voss <jvoss@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Conflicts:
    	init/devices.cpp

--- init/devices.cpp
+++ init/devices.cpp
@@ -378,6 +378,15 @@ static int find_vbd_device_prefix(const char *path, char *buf, ssize_t buf_sz)
     return 0;
 }
 
+static int find_bootdevice_prefix(const char *path, char *buf, ssize_t buf_sz)
+{
+    if (strstr(path, bootdevice)) {
+       strlcpy(buf, bootdevice, buf_sz);
+       return 0;
+    }
+    return -1;
+}
+
 static void parse_event(const char *msg, struct uevent *uevent)
 {
     uevent->action = "";
@@ -525,6 +534,9 @@ char** get_block_device_symlinks(struct uevent* uevent) {
     } else if (!find_vbd_device_prefix(uevent->path, buf, sizeof(buf))) {
         device = buf;
         type = "vbd";
+    } else if (!find_bootdevice_prefix(uevent->path, buf, sizeof(buf))) {
+        device = buf;
+        type = "platform";
     } else {
         return NULL;
     }
