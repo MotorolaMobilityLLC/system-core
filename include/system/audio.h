t2m-foxfone/platform_system_core

system/core: Add support for EVRCNW audio format
This change is needed to support EVRCNW audio format.

Change-Id: I161c2c24ca764ed291f59d17d0a2be443818a7aa

include/system/audio.h
@@ -165,6 +165,7 @@ typedef enum {
     AUDIO_FORMAT_DTS_LBR             = 0x13000000UL,
     AUDIO_FORMAT_AMR_WB_PLUS         = 0x14000000UL,
     AUDIO_FORMAT_MP2                 = 0x15000000UL,
+    AUDIO_FORMAT_EVRCNW              = 0x16000000UL,
     AUDIO_FORMAT_MAIN_MASK           = 0xFF000000UL,
     AUDIO_FORMAT_SUB_MASK            = 0x00FFFFFFUL,
 
@@ -612,6 +613,7 @@ static inline bool audio_is_valid_format(audio_format_t format)
     case AUDIO_FORMAT_DTS_LBR:
     case AUDIO_FORMAT_AMR_WB_PLUS:
     case AUDIO_FORMAT_MP2:
+    case AUDIO_FORMAT_EVRCNW:
         return true;
     default:
         return false;
@@ -630,6 +632,7 @@ static inline bool audio_is_supported_compressed(audio_format_t format)
         format == AUDIO_FORMAT_EVRC ||
         format == AUDIO_FORMAT_EVRCB ||
         format == AUDIO_FORMAT_EVRCWB ||
+        format == AUDIO_FORMAT_EVRCNW ||
         format == AUDIO_FORMAT_QCELP ||
         format == AUDIO_FORMAT_AAC)
         return true;

MotorolaMobilityLLC/system-core

(cherry picked from commit 96dada5496b0c2da352657047619607ada61259d)
Conflicts:
	include/system/audio.h

include/system/audio.h
@@ -273,6 +273,7 @@ typedef enum {
     AUDIO_FORMAT_DTS_LBR             = 0x15000000UL,
     AUDIO_FORMAT_AMR_WB_PLUS         = 0x16000000UL,
     AUDIO_FORMAT_MP2                 = 0x17000000UL,
+    AUDIO_FORMAT_EVRCNW              = 0x18000000UL,
     AUDIO_FORMAT_MAIN_MASK           = 0xFF000000UL,
     AUDIO_FORMAT_SUB_MASK            = 0x00FFFFFFUL,
 
@@ -1397,6 +1398,7 @@ static inline bool audio_is_valid_format(audio_format_t format)
     case AUDIO_FORMAT_DTS_LBR:
     case AUDIO_FORMAT_AMR_WB_PLUS:
     case AUDIO_FORMAT_MP2:
+    case AUDIO_FORMAT_EVRCNW:
         return true;
     default:
         return false;
@@ -1413,6 +1415,7 @@ static inline bool audio_is_supported_compressed(audio_format_t format)
     if (format == AUDIO_FORMAT_AMR_NB ||
         format == AUDIO_FORMAT_AMR_WB ||
         format == AUDIO_FORMAT_EVRC ||
+        format == AUDIO_FORMAT_EVRCNW ||
         format == AUDIO_FORMAT_QCELP ||
         format == AUDIO_FORMAT_AAC)
         return true;