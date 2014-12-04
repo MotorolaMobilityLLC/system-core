4667ce4a86c8c0e7284b605f4e3cb21c5909bf03
Lofus Lu <lofus@motorola.com>
2014-12-04 12:17:12 -0600

    Intercept adb install command for SecureIt
    Requirement: CTA spec (YDT 2407), when install apps via pc tools (adb) to mobile device, it
    should prompt user for awareness and confirmation. SecureIt app is designed to support this.
    
    SecureIt works to intercept the installation of apps, and present the UI to user. This patch
    enables the intercept for all adb installation commands.
    
    The patch is for China product line and thus its configured by macro ENABLE_LENOVO_SECUREIT
    which is enabled from product makefile.
    
    Change-Id: I54b497d33e5512f027e51b7eb43cb82510ca346f
    Reviewed-on: http://gerrit.mot.com/692395
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Yuanliang Lu <lofus@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- adb/Android.mk
+++ adb/Android.mk
@@ -117,6 +117,10 @@ LOCAL_SRC_FILES := \
 LOCAL_CFLAGS := -O2 -g -DADB_HOST=0 -Wall -Wno-unused-parameter -Werror
 LOCAL_CFLAGS += -D_XOPEN_SOURCE -D_GNU_SOURCE
 
+ifeq ($(ENABLE_LENOVO_SECUREIT),true)
+LOCAL_CFLAGS += -DCTA=1
+endif
+
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 LOCAL_CFLAGS += -DALLOW_ADBD_ROOT=1
 endif
