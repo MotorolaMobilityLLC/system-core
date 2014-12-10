df58cfefa77e0b93b0dd474e2c2e633c95ae6cd3
Ravi Vembu <raviv@motorola.com>
2014-12-10 10:51:51 -0600

    init: Update usb property before loading persist properties
    When encryption is turned on, the persist properties are loaded post
    decryption. This causes the USB mode to be overwritten with something
    that is not correct for the boot mode. Hence update the usb persist
    properties before loading/triggering all persist properties.
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    
    Change-Id: I0a75c6a24e9ad7642db3e119b60805c843786972
    Reviewed-on: http://gerrit.mot.com/696640
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    SLTApproved: Sanjay Dhar <sanjaydhar@motorola.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -573,6 +573,9 @@ void load_persist_props(void) {
 
     load_override_properties();
 
+    /* IKVPREL1L-4680 - update usb properties after decryption */
+    update_persistent_usb_property();
+
     /* Read persistent properties after all default values have been loaded. */
     load_persistent_properties();
 }
