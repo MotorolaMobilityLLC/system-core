02477d96dd0389652b94b1f4f50bf1bd762fbd0f
Dmitry Ornatsky <cdo021c@motorola.com>
2015-03-06 14:41:48 -0600

    system/core: filesystem_config: add mot dtv group
    add mot_dtv group for dtv specific SW configurations
    
    Change-Id: Iea5f9b122b7a44892892a73f342642e58aad062b
    Signed-off-by: Dmitry Ornatsky <cdo021c@motorola.com>
    Reviewed-on: http://gerrit.mot.com/722352
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Reviewed-by: Joel Voss <jvoss@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -120,6 +120,7 @@
 #define AID_FINGERP       9015  /* IKFPS-98 Add permission group for fingerprint */
 #define AID_MOT_ESDFS     9016  /* mot_esdfs for ESDFS package list parsing */
 #define AID_POWER         9017 /* power management */
+#define AID_MOT_DTV       9018 /* dtv */
 
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
 #define AID_MISC          9998  /* access to misc storage */
@@ -233,6 +234,7 @@ static const struct android_id_info android_ids[] = {
     { "mot_esdfs",     AID_MOT_ESDFS, },
     { "mot_esdfs",     AID_MOT_ESDFS, },
     { "power",         AID_POWER, },
+    { "mot_dtv",       AID_MOT_DTV, },
 };
 
 #define android_id_count \
