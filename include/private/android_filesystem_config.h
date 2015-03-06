f60831889712184c2697d0fc54c63481208cb66c
Dmitry Ornatsky <cdo021c@motorola.com>
2015-03-06 14:41:48 -0600

    system/core: filesystem_config: add mot dtv group
    add mot_dtv group for dtv specific SW configurations
    
    Change-Id: Iea5f9b122b7a44892892a73f342642e58aad062b
    Signed-off-by: Dmitry Ornatsky <cdo021c@motorola.com>
    Reviewed-on: http://gerrit.mot.com/722352
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Reviewed-by: Joel Voss <jvoss@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -155,6 +155,7 @@
 #define AID_MOT_DLNA      9011  /*DLNA native */
 #define AID_MOT_ATVC      9012  /* mot_atvc - This is for use of the ATVC service ONLY */
 #define AID_MOT_ESDFS     9016  /* mot_esdfs for ESDFS package list parsing */
+#define AID_MOT_DTV       9018 /* dtv */
 
 #define AID_MOT_DBVC      9014  /* mot_dbvc - This group is used to access DataBlock feature */
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
@@ -286,6 +287,7 @@ static const struct android_id_info android_ids[] = {
     { "mot_atvc",      AID_MOT_ATVC, },
     { "mot_dbvc",      AID_MOT_DBVC, },
     { "mot_esdfs",     AID_MOT_ESDFS, },
+    { "mot_dtv",       AID_MOT_DTV, },
 };
 
 #define android_id_count \
