c9bb5378319a5115b07d7fb4c28b8352ca5c4bba
Joel Voss <jvoss@motorola.com>
2015-03-16 04:51:43 -0500

    Fix ALLOW_DISABLE_SELINUX for secure builds
    Remove a temp workaround that allowed SELinux permissive mode via kernel
    cmdline option on signed user builds.
    
    Change-Id: Iee8633fbcc44c817bc49c410486cbca86fc1c2bd
    Signed-off-by: Joel Voss <jvoss@motorola.com>
    Reviewed-on: http://gerrit.mot.com/725274
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kevin Gudeth <kgudeth@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- init/Android.mk
+++ init/Android.mk
@@ -28,10 +28,9 @@ ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 LOCAL_CFLAGS += -DALLOW_LOCAL_PROP_OVERRIDE=1 -DALLOW_DISABLE_SELINUX=1
 else
 # Allow disabling SELinux for not signed user buids
-# IKGPEL-101 - wljv10 - STOPSHIP! Temporarily allow SELinux to be in permissive mode for user builds
-# ifneq ($(RADIO_SECURE),1)
+ifneq ($(RADIO_SECURE),1)
 LOCAL_CFLAGS += -DALLOW_DISABLE_SELINUX=1
-# endif
+endif
 endif
 
 # Enable ueventd logging
