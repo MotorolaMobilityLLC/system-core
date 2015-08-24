b6e964c1f89dfbb0903feeb6d415159a0b5283f9
Russ W. Knize <Russ.Knize@motorola.com>
2015-08-24 09:46:24 -0500

    add bootdevice symlinks
    Enumerate all boot device partitions as symlinks from /dev/block/bootdevice
    to their real block devices.  This allows different boot devices to be
    located transparently without the name of the physical device.
    
    Change-Id: I63bd8dd65ad609b2eeacf8b84911e9e1a39ce0a9
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/777888
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Dean Vogler <cdv001@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>

--- init/devices.cpp
+++ init/devices.cpp
@@ -48,6 +48,7 @@
 #include "ueventd_parser.h"
 #include "util.h"
 #include "log.h"
+#include "property_service.h"
 #include <zlib.h>
 
 #define SYSFS_PREFIX    "/sys"
@@ -56,6 +57,7 @@ static const char *firmware_dirs[] = { "/custom/etc/firmware",
                                        "/vendor/firmware",
                                        "/firmware/image" };
 
+static char *bootdevice;
 extern struct selabel_handle *sehandle;
 
 static int device_fd = -1;
@@ -555,6 +557,14 @@ static char **get_block_device_symlinks(struct uevent *uevent)
         link_num++;
     else
         links[link_num] = NULL;
+
+    if (!bootdevice) {
+        std::string property = property_get("ro.boot.bootdevice");
+        if (!property.empty())
+            bootdevice = strdup(property.c_str());
+    }
+    if (bootdevice && !strcmp(device, bootdevice))
+        make_link_init(link_path, "/dev/block/bootdevice");
 
     return links;
 }
