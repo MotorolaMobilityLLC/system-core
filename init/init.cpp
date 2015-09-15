850efa3835c4a49b8bc241db5d73c41b18313961
Hong-Mei Li <a21834@motorola.com>
2015-09-15 15:32:23 -0700

    init: export ro.revision
    Android M deleted the previous ro.revision parse and setting mechanism,
    instead, it depends on androidboot.revision to get the property and provide
    a new ro.hw.hwrev to replace the old one.
    
    However, our boodloader dose not pass revision information, so that we get
    and empty prop value. Since Moto MD needs ro.revision, we parse both the
    androidboot.revision and ro.boot.hwrev, and set a readable ro.revision.
    
    Change-Id: Ibbc7cf5811ab6b9351d9c94309d7413bafc3b0f6
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.mot.com/783763
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- init/init.cpp
+++ init/init.cpp
@@ -520,7 +520,6 @@ static void export_kernel_boot_props() {
         { "ro.boot.baseband",   "ro.baseband",   "unknown", },
         { "ro.boot.bootloader", "ro.bootloader", "unknown", },
         { "ro.boot.hardware",   "ro.hardware",   "unknown", },
-        { "ro.boot.revision",   "ro.revision",   "0", },
         { "ro.boot.radio", "ro.hw.radio", NULL, },
         { "ro.boot.carrier", "ro.carrier", NULL, },
         { "ro.boot.device", "ro.hw.device", NULL, },
@@ -537,6 +536,27 @@ static void export_kernel_boot_props() {
         else if (prop_map[i].default_value != NULL)
             property_set(prop_map[i].dst_prop, prop_map[i].default_value);
     }
+
+    std::string tmp;
+    std::size_t found;
+    tmp = property_get("ro.boot.revision");
+    if (tmp.empty())
+        tmp = property_get("ro.hw.hwrev");
+    if (!tmp.empty()) {
+        found = tmp.find("0x");
+        if (found!=std::string::npos) {
+            tmp.erase(found, 2);
+            std::transform(tmp.begin(), tmp.end(), tmp.begin(), tolower);
+        }
+        switch(tmp[0]){
+            case '1': tmp[0] = 's'; break;
+            case '2': tmp[0] = 'm'; break;
+            case '8': tmp[0] = 'p'; break;
+            case '9': tmp[0] = 'd'; break;
+        }
+        property_set("ro.revision", tmp.c_str());
+    }
+
     export_kernel_boot_reason();
 }
 
