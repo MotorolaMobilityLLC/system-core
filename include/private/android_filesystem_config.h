da892ffc4616a0a93b07735888213dbaacd2a5f9
Jatin Majmudar <fjm225@motorola.com>
2015-10-15 11:47:20 -0500

    Add UID for ATVC
    Change include:
    This change was not ported to M line for some reason.
    
    Change-Id: I039f3ff87a72b89c1c309f1a54e6e6ca587ff385
    Reviewed-on: http://gerrit.mot.com/791811
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Mark Reed <mreed@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -173,6 +173,7 @@
 #define AID_MOT_WHISPER   9009  /* Whisper Protocol access */
 #define AID_MOT_CAIF      9010  /* can create CAIF sockets */
 #define AID_MOT_DLNA      9011  /*DLNA native */
+#define AID_MOT_ATVC      9012  /* mot_atvc - This is for use of the ATVC service ONLY */
 #define AID_MOT_ESDFS     9016  /* mot_esdfs for ESDFS package list parsing */
 
 #define AID_MOT_DBVC      9014  /* mot_dbvc - This group is used to access DataBlock feature */
@@ -332,6 +333,7 @@ static const struct android_id_info android_ids[] = {
     { "mot_whisper",	AID_MOT_WHISPER, },
     { "mot_caif",	AID_MOT_CAIF, },
     { "mot_dlna",	AID_MOT_DLNA, },
+    { "mot_atvc",      AID_MOT_ATVC, },
     { "mot_dbvc",      AID_MOT_DBVC, },
     { "mot_esdfs",     AID_MOT_ESDFS, },
 };
