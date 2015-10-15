2423aae8b96ab9e8c030849494f8425434f3701c
Jatin Majmudar <fjm225@motorola.com>
2015-10-15 11:47:20 -0500

    Add UID for ATVC
    Change include:
    This change was not ported to M line for some reason.
    
    Change-Id: I039f3ff87a72b89c1c309f1a54e6e6ca587ff385
    Reviewed-on: http://gerrit.mot.com/791811
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Mark Reed <mreed@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -115,6 +115,7 @@
 #define AID_MOT_WHISPER   9009  /* Whisper Protocol access */
 #define AID_MOT_CAIF      9010  /* can create CAIF sockets */
 #define AID_MOT_DLNA      9011  /*DLNA native */
+#define AID_MOT_ATVC      9012  /* mot_atvc - This is for use of the ATVC service ONLY */
 #define AID_SPRINT_EXTENSION 9013  /* IKASANTISPRINT-149 sprint extension service */
 #define AID_MOT_DBVC      9014  /* mot_dbvc - This group is used to access DataBlock feature related data */
 #define AID_FINGERP       9015  /* IKFPS-98 Add permission group for fingerprint */
@@ -233,6 +234,7 @@ static const struct android_id_info android_ids[] = {
     { "mot_whisper",	AID_MOT_WHISPER, },
     { "mot_caif",	AID_MOT_CAIF, },
     { "mot_dlna",	AID_MOT_DLNA, },
+    { "mot_atvc",      AID_MOT_ATVC, },
     { "sprint_extension", AID_SPRINT_EXTENSION, },
     { "mot_dbvc",      AID_MOT_DBVC, },
     { "fingerp",        AID_FINGERP, },
