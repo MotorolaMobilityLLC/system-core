fa9465bcc03fe7639dbfdd5df63ac95d85785cb3
Hong-Mei Li <a21834@motorola.com>
2015-12-22 00:32:55 -0800

    IKSWM-14685 init: re-load persist props
    Since encrypted device would mostly miss the persist props
    setting, we reload persist.* from /system/build.prop only.
    
    This would avoid ro.* load failed warnnings, and reduce boot
    up time a little.
    
    For the other properties, let's monitor and handle them case
    by case if there is any issue after encryption.
    
    Change-Id: Ife54e250542365e404d31286968105b2aa89284f
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.mot.com/808954
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -603,7 +603,7 @@ void load_persist_props(void) {
      * to be overwrite by default values.
      */
     persistent_properties_loaded = 0;
-    load_properties_from_file(PROP_PATH_SYSTEM_BUILD, NULL);
+    load_properties_from_file(PROP_PATH_SYSTEM_BUILD, "persist.*");
     /* END Motorola Hong-Mei Li 2012-09-10, IKJBREL1-5477 */
 
     load_override_properties();
