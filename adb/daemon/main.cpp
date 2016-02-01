b6b567ea671552f94e8f646a89861fac871007ce
Jatin Majmudar <fjm225@motorola.com>
2016-02-01 16:15:26 -0600

    IKSWM-19121 property name change for debug token
    Changes include:
    a) Support ro.boot.adb_root for newer product
    b) Keep existing property name for older products.
    
    Change-Id: I23c78df512b188b37a9fa031853f608b4d556076
    Reviewed-on: http://gerrit.mot.com/817776
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joel Voss <jvoss@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- adb/daemon/main.cpp
+++ adb/daemon/main.cpp
@@ -110,6 +110,10 @@ static bool should_drop_privileges() {
         if (strcmp(value, "1") == 0) {
             drop = false;
         }
+        property_get("ro.boot.adb_root", value, "");
+        if (strcmp(value, "enable") == 0) {
+            drop = false;
+        }
     }
     /* END IKSECURITY-199 */
     return drop;
@@ -121,6 +125,10 @@ static bool should_drop_privileges() {
     value[0] = 0x00;
     property_get("persist.atvc.adb", value, "");
     if (strcmp(value, "1") == 0) {
+        drop = false;
+    }
+    property_get("ro.boot.adb_root", value, "");
+    if (strcmp(value, "enable") == 0) {
         drop = false;
     }
     return drop; // "adb root" not allowed, always drop privileges.
