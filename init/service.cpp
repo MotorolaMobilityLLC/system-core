bc281c9fb0d1b8ce21b92ae3012835c8d9248075
Yinjun Chen <a7301c@motorola.com>
2016-03-13 18:24:41 -0500

    IKQCOMSD1-3208: N-bringup-HACK: Keep starting process even domain transistion is not defined
    Google commit from N as below will not start the process if there is no domain
    transistion defined.
    4800dbf init: refuse to start process if domain transition not defined
    
    This would cause thousands of process does not get start.
    e.g. adb is not working due to mmi-usb-sh is refused to start.
    
    Change-Id: Id7e6b7fdeba46cf42374af7ec78cd50f4b78cad1

--- init/service.cpp
+++ init/service.cpp
@@ -376,9 +376,10 @@ bool Service::Start() {
         }
         if (rc == 0 && scon == mycon) {
             ERROR("Service %s does not have a SELinux domain defined.\n", name_.c_str());
-            free(mycon);
-            free(fcon);
-            return false;
+            //MMI_STOPSHIP Keep starting process even domain transistion is not defined for N bringup.
+            //free(mycon);
+            //free(fcon);
+            //return false;
         }
         free(mycon);
         free(fcon);
