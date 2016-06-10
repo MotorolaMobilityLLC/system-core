805d4cffe428ec3d1f56120d1032b5cdb28d3602
Donghua Yan <a18317@motorola.com>
2016-06-10 10:29:54 -0500

    fastboot: Support "oem ramdump pull" command
    fastboot oem ramdump pull [[all]|<file_name>]
    
    Change-Id: Icf3e56970401d5ba2c07c73acbce02228460f831
    Reviewed-on: https://gerrit.mot.com/1013030
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Qing Chang <qing@motorola.com>
    Submit-Approved: Jira Key

--- fastboot/protocol.cpp
+++ fastboot/protocol.cpp
@@ -34,6 +34,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include <algorithm>
 #include <vector>
@@ -431,6 +434,134 @@ int fb_dump_data(Transport *transport, const char *file_name)
 
     if (check_response(transport, 0, 0) < 0)
         return -1;
+
+    return 0;
+}
+
+static int dir_exist(const char *dir_name)
+{
+    struct stat st;
+
+    if (stat(dir_name, &st) == 0) {
+        if (S_ISDIR(st.st_mode)) {
+            return 1;
+        } else
+            return -1;
+     } else {
+            return 0;
+     }
+}
+
+#define RAM_DUMP_DIR    "ramdump"
+#ifdef _WIN32
+#define FILE_SPLIT      "\\"
+#else
+#define FILE_SPLIT      "/"
+#endif
+
+static int create_ramdump_dir(void)
+{
+    int ret;
+
+    /* search and rename old ramdump file */
+    if ((ret = dir_exist(RAM_DUMP_DIR)) == 1) {
+        /* rename the directory */
+        int i = 0;
+        while (1) {
+            char new_name[16] = {0};
+
+            sprintf(new_name, "%s_%d", RAM_DUMP_DIR, i++);
+            if (dir_exist(new_name) == 0) {
+                if (rename(RAM_DUMP_DIR, new_name) == 0)
+                    break;
+                else {
+                    g_error = android::base::StringPrintf("failed to rename %s to %s: %s", RAM_DUMP_DIR, new_name, strerror(errno));
+                    return -1;
+                }
+            }
+        }
+    } else if (ret == -1) {
+        g_error = android::base::StringPrintf("non-directory %s exists, please rename it", RAM_DUMP_DIR);
+        return -1;
+    }
+
+    /* create dir */
+#ifdef _WIN32
+    if (mkdir(RAM_DUMP_DIR) == -1) {
+#else
+    if (mkdir(RAM_DUMP_DIR, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1) {
+#endif
+        g_error = android::base::StringPrintf("failed to create directory %s: %s", RAM_DUMP_DIR, strerror(errno));
+        return -1;
+    }
+
+    return 0;
+}
+
+/*
+ * function fb_ramdump_files is used to dump the file[s] from phone side.
+ * fastboot will dump the ram files one by one according to the received
+ * file list in fb_query_ramdump_files.
+ */
+int fb_dump_ram_files(Transport *transport)
+{
+    int ret = 0;
+    char cmd_buffer[64] = {0};
+
+    /* create the ramdump folder */
+    ret= create_ramdump_dir();
+    if (ret < 0) {
+        transport->Close();
+        return -1;
+    }
+
+    while (true) {
+        int file_name_len;
+        char ram_file[24] = {0};
+        char file_name[32] = {0};
+
+        /* get ram file name: DUMP%04xfile */
+        ret = transport->Read(cmd_buffer, 64);
+        if(ret < 0) {
+            g_error = android::base::StringPrintf("status read failed (%s)", strerror(errno));
+            transport->Close();
+            return -1;
+        }
+        if (ret < 8) {
+            g_error = android::base::StringPrintf("invalid dump command (%s)", cmd_buffer);
+            transport->Close();
+            return -1;
+        }
+
+        if (strcmp(cmd_buffer, "DUMPDONE") == 0) {
+            return check_response(transport, 0, 0);
+        }
+
+        if (sscanf(cmd_buffer, "DUMP%04x", &file_name_len) == EOF) {
+            g_error = android::base::StringPrintf("invalid protocol(%s)", cmd_buffer);
+            transport->Close();
+            return -1;
+        }
+
+        memcpy(ram_file, cmd_buffer + 8, file_name_len);
+        snprintf(file_name, 32, "%s%s%s", RAM_DUMP_DIR, FILE_SPLIT, ram_file);
+
+        /* response "OKAY" to sync the protocol */
+        memset(cmd_buffer, 0, 64);
+        sprintf(cmd_buffer, "OKAY");
+        if (transport->Write("OKAY", 4) < 0) {
+            transport->Close();
+            return -1;
+        }
+
+        fprintf(stderr, "Receiving \"%s\"...\n", ram_file);
+
+        /* start to recive file */
+        if (dump_file(transport, file_name) == -1) {
+            sprintf(cmd_buffer, "failed to dump ram file %s", ram_file);
+            return -1;
+        }
+    }
 
     return 0;
 }
