8c3fc659a73dac3a360e4df308707e5753a990b1
Donghua Yan <a18317@motorola.com>
2016-06-10 10:40:47 -0500

    IKSWO-548 fastboot: add support for 'fastboot oem partition dump'
    Change-Id: I228dd2dcad2ab3203c0459b31ce7c4213e1e98fa
    Reviewed-on: https://gerrit.mot.com/1013031
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Qing Chang <qing@motorola.com>
    Submit-Approved: Jira Key

--- fastboot/moto_handler.cpp
+++ fastboot/moto_handler.cpp
@@ -73,3 +73,29 @@ int oem_ramdump_handler(const std::string& cmd, std::vector<std::string>* args)
 
     return 0;
 }
+
+int oem_partition_handler(const std::string& cmd, std::vector<std::string>* args)
+{
+    if (args->empty()) die("empty oem command");
+    if (args->size() < 3) die("Invalid command: fastboot oem partition dump <partition> [size] [offset]");
+
+    int is_dump = 0;
+
+    if (args->at(1) == std::string("dump")) {
+        /* translate "pull" to "moto-pull" */
+        args->at(1) = std::string("moto-dump");
+        is_dump = 1;
+    }
+
+    std::string command(cmd);
+    std::string partition = args->at(2);
+    while (!args->empty()) {
+        command += " " + next_arg(args);
+    }
+
+    fb_queue_command(command.c_str(), "Sending command");
+    if (is_dump)
+        fb_queue_dump(partition);
+
+    return 0;
+}
