f80466a3e5d96649f51c8da11e1e13ee742483ab
spooja <spooja@codeaurora.org>
2016-06-16 12:19:25 +0530

    Enable core dump generation for native processes
    PROPAGATED_from (CR)
    
    This change will enable core dump generation for
    all native processes, based on the property
    persist.debug.trace.
    
    CRs-Fixed:2300284
    Mot-CRs-Fixed:(CR)
    
    Change-Id: Icaa4d456952ea95dbecc6911a082509538e6786a
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: Robert Cabral <robertc@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2026962
    Reviewed-by: Marcos Sassahara <msassaha@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2374004
    Submit-Approved: Gilberto Gambugge Neto <gambugge@motorola.com>

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1111,11 +1111,23 @@ static int check_rlim_action() {
         if (setrlimit(RLIMIT_CORE, &rl) < 0) {
             PLOG(ERROR) << "could not enable core file generation";
         }
+
     }
     return 0;
 }
 
 static Result<void> do_load_persist_props(const BuiltinArguments& args) {
+    // Devices with FDE have load_persist_props called twice; the first time when the temporary
+    // /data partition is mounted and then again once /data is truly mounted.  We do not want to
+    // read persistent properties from the temporary /data partition or mark persistent properties
+    // as having been loaded during the first call, so we return in that case.
+    std::string crypto_state = android::base::GetProperty("ro.crypto.state", "");
+    std::string crypto_type = android::base::GetProperty("ro.crypto.type", "");
+    if (crypto_state == "encrypted" && crypto_type == "block") {
+        static size_t num_calls = 0;
+        if (++num_calls == 1) return {};
+    }
+
     SendLoadPersistentPropertiesMessage();
 
     start_waiting_for_property("ro.persistent_properties.ready", "true");
