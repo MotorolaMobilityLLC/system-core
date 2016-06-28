4974f0fcaacc2a2c1dc775f4464e9a53df8a4511
Hong-mei Li <a21834@motorola.com>
2016-06-28 10:38:44 -0700

    logd: Fix race between flushTo and prune
    During logd.reader.per send msg data to the socket, it would unlock
    mLogElementLock. In case there are lots of logs, maybePrune would be
    triggered to erase/delete some elements from the list.
    And then, when logd.reader.per back from send socket and go on scanning
    the list next, it may get an corrupted pointer.
    
    Fix this by
    1. backup the current iterator content before unlock
    2. dirty the memory before erase iterator
    3. compare the content of the iterator and backup value, go to next
       only when no change detected.
    
    Change-Id: I14574eb02551ef66916cd1d187b9229cd43494c9
    Signed-off-by: Hong-mei Li <a21834@motorola.com>
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.mot.com/871381
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -241,6 +241,12 @@ void LogBuffer::maybePrune(log_id_t id) {
     }
 }
 
+void LogBuffer::poison(
+        LogBufferElementCollection::iterator it) {
+    unsigned long *it_addr = (unsigned long *)((unsigned long)&(*it));
+    memset(it_addr, 0x6b, sizeof(unsigned long));
+}
+
 LogBufferElementCollection::iterator LogBuffer::erase(
         LogBufferElementCollection::iterator it, bool engageStats) {
     LogBufferElement *e = *it;
@@ -256,6 +262,7 @@ LogBufferElementCollection::iterator LogBuffer::erase(
     log_id_for_each(i) {
         doSetLast |= setLast[i] = mLastSet[i] && (it == mLast[i]);
     }
+    poison(it);
     it = mLogElements.erase(it);
     if (doSetLast) {
         log_id_for_each(i) {
@@ -732,6 +739,7 @@ uint64_t LogBuffer::flushTo(
     LogBufferElementCollection::iterator it;
     uint64_t max = start;
     uid_t uid = reader->getUid();
+    unsigned long oldit_value;
 
     pthread_mutex_lock(&mLogElementsLock);
 
@@ -753,6 +761,8 @@ uint64_t LogBuffer::flushTo(
 
     for (; it != mLogElements.end(); ++it) {
         LogBufferElement *element = *it;
+
+        oldit_value = (unsigned long)*it;
 
         if (!privileged && (element->getUid() != uid)) {
             continue;
@@ -783,6 +793,12 @@ uint64_t LogBuffer::flushTo(
         }
 
         pthread_mutex_lock(&mLogElementsLock);
+
+        if (oldit_value != (unsigned long)*it) {
+            pthread_mutex_unlock(&mLogElementsLock);
+            return max;
+        }
+
     }
     pthread_mutex_unlock(&mLogElementsLock);
 
