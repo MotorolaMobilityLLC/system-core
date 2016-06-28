bd2148c30271e4c308b5824038a2da4814cdad25
Hong-mei Li <a21834@motorola.com>
2016-06-28 10:38:44 -0700

    logd: Fix race between flushTo and prune
    propagated from : (CR)
    
    During logd.reader.per send msg data to the socket, it would unlock
    mLogElementLock. In case there are lots of logs, maybePrune would be
    triggered to erase/delete some elements from the list.
    And then, when logd.reader.per back from send socket and go on scanning
    the list next, it may get a corrupted pointer.
    
    Fix this by
    1. backup the current iterator content before unlock
    2. dirty the memory before erase iterator
    3. compare the content of the iterator and backup value, go to next
       only when no change detected.
    
    Change-Id: I14574eb02551ef66916cd1d187b9229cd43494c9
    Signed-off-by: Hong-mei Li <a21834@motorola.com>
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.mot.com/871381
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.mot.com/892011

--- logd/LogBuffer.h
+++ logd/LogBuffer.h
@@ -190,6 +190,7 @@ class LogBuffer : public LogBufferInterface {
     bool prune(log_id_t id, unsigned long pruneRows, uid_t uid = AID_ROOT);
     LogBufferElementCollection::iterator erase(
         LogBufferElementCollection::iterator it, bool coalesce = false);
+    void poison(LogBufferElementCollection::iterator it);
 };
 
 #endif  // _LOGD_LOG_BUFFER_H__
