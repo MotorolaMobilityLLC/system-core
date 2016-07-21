f31aba105932417333621a30391ac3cb9cf664cc
Konstantin Makariev <kmakariev@motorola.com>
2016-07-21 14:16:18 -0500

    IKSWN-4119 init: validate carrier compatibility
    This change validates carrier compatibility of system and oem images.
    It uses properties ro.carrier.subsidized and ro.carrier.oem located in
    build.prop and oem.prop correspondingly. These optional properties
    define possible values of ro.carrier property passed via
    kernel command line androidboot.carrier argument.
    
    If property ro.carrier.sibsidized exists and contains value of
    ro.carrier property, then it's required that property ro.carrier.oem
    also had value of ro.carrier property to allow kernel boot. Otherwise
    phone will boot into recovery mode.
    
    Propagation of IKSWM-52148
    
    Change-Id: I12e3644a4fe257c655871cb8315d7a183c75013d
    Signed-off-by: Konstantin Makariev <kmakariev@motorola.com>
    Reviewed-on: http://gerrit.mot.com/879834
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kevin Spriggs <kspriggs@motorola.com>
    Reviewed-on: https://gerrit.mot.com/898822
    (cherry picked from commit 081a5cdf7684d2d0e7523e4c96207db5583afed0)

--- init/hw_tags.h
+++ init/hw_tags.h
@@ -4,6 +4,7 @@
 #include <sys/system_properties.h>
 
 int process_hw_mappings(const char *xml_name);
+void verify_carrier_compatibility(void);
 
 extern int __system_property_get(const char *name, char *value);
 extern int property_set(const char *name, const char *value);
