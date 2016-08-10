047975fddc22171d67129f2d8ff6217fd44d37b5
Robert Wiegand <qa2113@motorola.com>
2016-08-10 12:56:17 -0500

    Restore AID_SENSORS
    AID_SENSORS was removed from the Qcomm code since it wasn't used
    on phones with a Moto sensor hub. Since we will be using the
    Qcomm sensor support in future products it needs to be put back.
    
    Change-Id: If95607e04f0bc790f939153909aacc63ed7ddfab
    Reviewed-on: /886361
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Jae Hyun Koh <jasonkoh@motorola.com>
    Reviewed-by: Eric Hefner <ehefner@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -125,6 +125,7 @@
 /* The range 5000-5999 is also reserved for OEM, and must never be used here. */
 #define AID_OEM_RESERVED_2_START 5000
 #define AID_OEM_RESERVED_2_END   5999
+#define AID_SENSORS       3011 /* access to /dev/socket/sensor_ctl_socket & QCCI/QCSI */
 
 #define AID_MOT_ACCY      9000  /* access to accessory */
 #define AID_MOT_PWRIC     9001  /* power IC */
@@ -246,6 +247,7 @@ static const struct android_id_info android_ids[] = {
     { "net_bt_stack",  AID_NET_BT_STACK, },
     { "readproc",      AID_READPROC, },
     { "wakelock",      AID_WAKELOCK, },
+    { "sensors",       AID_SENSORS, },
 
     { "rfs",           AID_RFS, },
     { "rfs_shared",    AID_RFS_SHARED, },
