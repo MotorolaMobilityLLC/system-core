246b0509ca383e9ce78e35dbcf1e9c8ab3a3218c
quebs2 <quebs2@lenovo.com>
2016-09-19 12:27:02 +0800

    -Fix bootdevice link creation with sdcard inserted.
    Create bootdevice by-name/by-num links only if the block
    device is the bootdevice which was explictly specified by
    linking to internal eMMC storage rather than external SD
    card. This fixes the issue of bootdevice path being created
    incorrectly if the first reported device with by-name or
    by-num is not the expected bootdevice. Note that previous
    commit change from https://gerrit.mot.com/#/c/777888/ will
    just block init to process specified event actions that we
    expect and as a consequence the external partition with
    REAL directory /dev/block/bootdevice/by-name would be then
    created instead without the condition we here attempt to
    perform, which subsequently caused librpmb.so could not
    locate the mmcblk0rpmb partition as the /dev/block/bootdevice
    is not a symbol link any more. Most likely, the event here
    with sdcard inserted that reports to userspace for underlying
    handles is ahead of the eMMC internal storage enumeration.
    
    Change-Id: I7d479152f7a8a04936bab206f46547950ea10057
    Signed-off-by: quebs2 <quebs2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/900628
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jiang-Li Yuan <jlyuan@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1020255
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Coverity Analysis <coverity@motorola.com>
    Reviewed-by: Binsheng Que <quebs2@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- init/devices.cpp
+++ init/devices.cpp
@@ -561,6 +561,7 @@ char** get_block_device_symlinks(struct uevent* uevent) {
     char link_path[256];
     int link_num = 0;
     char *p;
+    static int is_bootdevice_linked = 0;
 
     pdev = find_platform_device(uevent->path);
     if (pdev) {
@@ -595,10 +596,12 @@ char** get_block_device_symlinks(struct uevent* uevent) {
             link_num++;
         else
             links[link_num] = NULL;
-        if (asprintf(&links[link_num], "/dev/block/bootdevice/by-name/%s", p) > 0)
-            link_num++;
-        else
-            links[link_num] = NULL;
+        if (is_bootdevice_linked && !boot_device.empty() && strstr(device, boot_device.c_str())) {
+            if (asprintf(&links[link_num], "/dev/block/bootdevice/by-name/%s", p) > 0)
+                link_num++;
+            else
+                links[link_num] = NULL;
+        }
 
         free(p);
     }
@@ -609,10 +612,12 @@ char** get_block_device_symlinks(struct uevent* uevent) {
         else
             links[link_num] = NULL;
 
-        if (asprintf(&links[link_num], "/dev/block/bootdevice/by-num/p%d", uevent->partition_num) > 0)
-            link_num++;
-        else
-            links[link_num] = NULL;
+        if (is_bootdevice_linked && !boot_device.empty() && strstr(device, boot_device.c_str())) {
+            if (asprintf(&links[link_num], "/dev/block/bootdevice/by-num/p%d", uevent->partition_num) > 0)
+                link_num++;
+            else
+                links[link_num] = NULL;
+        }
     }
 
     slash = strrchr(uevent->path, '/');
@@ -624,6 +629,7 @@ char** get_block_device_symlinks(struct uevent* uevent) {
     if (pdev && !boot_device.empty() && strstr(device, boot_device.c_str())) {
         /* Create bootdevice symlink for platform boot stroage device */
         make_link_init(link_path, "/dev/block/bootdevice");
+        is_bootdevice_linked = 1;
     }
 
     return links;
