ba90bdc45658f02c327af9cc581e5a34c8fb20a1
Mark Salyzyn <salyzyn@google.com>
2016-10-21 09:46:42 -0700

    logd: mLastWorstPidOFSystem crash
    (cherry pick from commit fa07f9dc4b4d101a49fba5dbbf35c88cdfec4433)
    
    mLastWorstPidOfSystem is filled with iterator references
    that are not from AID_SYSTEM to aid the performance. But
    we only clear entries from the list during erase if they
    are from AID_SYSTEM. Remove the filter check in erase so
    the stale references will be removed.
    
    The conditions that caused this failure are difficult to
    reproduce and are rare.
    
    Test: gTests logd-unit-tests, liblog-unit-tests and logcat-unit-tests
    Bug: 32247044
    Bug: 31237377
    Change-Id: Ie405dd643203b816cac15eef5c97600551cee450
    
    Mot-CRs-fixed: (CR)
    Change-Id: Ie405dd643203b816cac15eef5c97600551cee450
    Reviewed-on: https://gerrit.mot.com/921782
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -319,6 +319,9 @@ LogBufferElementCollection::iterator LogBuffer::erase(
     LogBufferElement *element = *it;
     log_id_t id = element->getLogId();
 
+    // Remove iterator references in the various lists that will become stale
+    // after the element is erased from the main logging list.
+
     {   // start of scope for uid found iterator
         LogBufferIteratorMap::iterator found =
             mLastWorstUid[id].find(element->getUid());
@@ -328,8 +331,8 @@ LogBufferElementCollection::iterator LogBuffer::erase(
         }
     }
 
-    if (element->getUid() == AID_SYSTEM) {
-        // start of scope for pid found iterator
+    {   // start of scope for pid found iterator
+        // element->getUid() may not be AID_SYSTEM for next-best-watermark.
         LogBufferPidIteratorMap::iterator found =
             mLastWorstPidOfSystem[id].find(element->getPid());
         if ((found != mLastWorstPidOfSystem[id].end())
@@ -646,6 +649,7 @@ bool LogBuffer::prune(log_id_t id, unsigned long pruneRows, uid_t caller_uid) {
                 ++it;
                 continue;
             }
+            // below this point element->getLogId() == id
 
             if (leading && (!mLastSet[id] || ((*mLast[id])->getLogId() != id))) {
                 mLast[id] = it;
@@ -698,6 +702,8 @@ bool LogBuffer::prune(log_id_t id, unsigned long pruneRows, uid_t caller_uid) {
                         && ((!gc && (element->getPid() == worstPid))
                             || (mLastWorstPidOfSystem[id].find(element->getPid())
                                 == mLastWorstPidOfSystem[id].end()))) {
+                    // element->getUid() may not be AID_SYSTEM, next best
+                    // watermark if current one empty.
                     mLastWorstPidOfSystem[id][element->getPid()] = it;
                 }
                 if ((!gc && !worstPid && (element->getUid() == worst))
@@ -716,6 +722,8 @@ bool LogBuffer::prune(log_id_t id, unsigned long pruneRows, uid_t caller_uid) {
                 ++it;
                 continue;
             }
+            // key == worst below here
+            // If worstPid set, then element->getPid() == worstPid below here
 
             pruneRows--;
             if (pruneRows == 0) {
@@ -739,6 +747,8 @@ bool LogBuffer::prune(log_id_t id, unsigned long pruneRows, uid_t caller_uid) {
                     if (worstPid && (!gc
                                 || (mLastWorstPidOfSystem[id].find(worstPid)
                                     == mLastWorstPidOfSystem[id].end()))) {
+                        // element->getUid() may not be AID_SYSTEM, next best
+                        // watermark if current one empty.
                         mLastWorstPidOfSystem[id][worstPid] = it;
                     }
                     if ((!gc && !worstPid) || (mLastWorstUid[id].find(worst)
