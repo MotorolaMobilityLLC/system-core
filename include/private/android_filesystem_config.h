ffe1f9487f1ce1b829ba7d79f486a0e287a75cae
qing yang <yangqing2@lenovo.com>
2016-12-27 11:31:32 +0800

    (CR): Add AID_MOT_MOD definition for Bug2Go
    Bug2Go depends on AID_MOT_MOD definition to pass build.
    
    Change-Id: I4614670a3bf22582dfd66c44db033a7ab18b5429
    Reviewed-on: https://gerrit.mot.com/935182
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: benmin lu <lubm1@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -121,6 +121,8 @@
 #define AID_OEM_RESERVED_2_START 5000
 #define AID_OEM_RESERVED_2_END   5999
 
+#define AID_MOT_MOD       9020 /* can interact with motorola mod HW */
+
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
@@ -220,6 +222,7 @@ static const struct android_id_info android_ids[] = {
     { "everybody",     AID_EVERYBODY, },
     { "misc",          AID_MISC, },
     { "nobody",        AID_NOBODY, },
+    { "mot_mod",       AID_MOT_MOD, },
 };
 
 #define android_id_count \
