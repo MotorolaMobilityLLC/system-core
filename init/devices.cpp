ca1ae801a706f2b1eb753020e34ad5abf0043c15
wangwang1 <wangwang@motorola.com>
2017-02-11 10:36:36 +0800

    Revert "(CR) init: Handle block device uevent before platform uevent"
    This reverts commit aabdd1c5ac641e7e0070e6380c6d17aa736a0419.
    
    Conflicts:
    	init/devices.cpp
    
    Change-Id: I7d1e544a0273a235c429c99a36226fc0270c2b38

--- init/devices.cpp
+++ init/devices.cpp
@@ -378,15 +378,6 @@ static int find_vbd_device_prefix(const char *path, char *buf, ssize_t buf_sz)
     return 0;
 }
 
-static int find_bootdevice_prefix(const char *path, char *buf, ssize_t buf_sz)
-{
-    if (strstr(path, bootdevice)) {
-       strlcpy(buf, bootdevice, buf_sz);
-       return 0;
-    }
-    return -1;
-}
-
 static void parse_event(const char *msg, struct uevent *uevent)
 {
     uevent->action = "";
@@ -569,9 +560,6 @@ char** get_block_device_symlinks(struct uevent* uevent) {
     } else if (!find_vbd_device_prefix(uevent->path, buf, sizeof(buf))) {
         device = buf;
         type = "vbd";
-    } else if (!find_bootdevice_prefix(uevent->path, buf, sizeof(buf))) {
-        device = buf;
-        type = "platform";
     } else {
         return NULL;
     }
