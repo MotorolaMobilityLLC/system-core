ed600e8390ca64b75662835cc24a989bf95f086b
Yun Ma <yunma@motorola.com>
2017-03-30 16:08:37 -0500

    - Add mot_dbvc usr for CID provisioning
        Change-Id: I80642ec0eb3f0aad275947e0d6c15d2be8d7a89f
        Reviewed-on: http://gerrit.pcs.mot.com/575552
        SLTApproved: Slta Waiver <sltawvr@motorola.com>
        Tested-by: Jira Key <jirakey@motorola.com>
        Reviewed-by: Dmitry Sokolov <dima@motorola.com>
        Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
        Submit-Approved: Jason Hrycay <jason.hrycay@motorola.com>
    
    Change-Id: I96a1aa74315a6bdbf511c03d171135e062eafcd5
    Signed-off-by: zhaoxp3 <zhaoxp3@motorola.com>

--- libcutils/include/private/android_filesystem_config.h
+++ libcutils/include/private/android_filesystem_config.h
@@ -173,6 +173,8 @@
 #define AID_MOT_CAIF      9010  /* can create CAIF sockets */
 #define AID_MOT_DLNA      9011  /*DLNA native */
 
+#define AID_MOT_DBVC      9014  /* mot_dbvc - This group is used to access DataBlock feature */
+
 #define AID_EVERYBODY 9997 /* shared between all apps in the same profile */
 #define AID_MISC 9998      /* access to misc storage */
 #define AID_NOBODY 9999
