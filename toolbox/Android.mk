08be7714475e55c7d6a14bfc3578d412d5949ba0
yelw1 <yelw1@lenovo.com>
2017-04-28 14:57:34 +0800

    INDRIYAPRO-1782 : cherry-pick (CR): Re-enable support for sendevent2 on JB
    This change adds back support for optimized sendevent command
    to toolbox.
    
    Ported from (CR)
    
    Change-Id: I01658995d5959577c709aa4a1e9cb17da54ac286
    Reviewed-on: http://gerrit.pcs.mot.com/462718
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Beylul Bahta <e50097@motorola.com>
    (cherry picked from commit 3582dba689969ee5c31701b7e62783f076c09ba2)
    
    Conflicts:
    	toolbox/Android.mk
    Change-Id: Ia575e1422ed5a2ccb5a34f17fcdfbab49e288ed1
    Reviewed-on: https://gerrit.mot.com/986676
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jianlin Nie <niejl1@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -45,6 +45,14 @@ OUR_TOOLS := \
     top \
 
 ALL_TOOLS = $(BSD_TOOLS) $(OUR_TOOLS)
+# Motorola - BEGIN - IKAPP-606 - wqnt78 - 3/10/2010 - apython enabler
+
+# Note: sendevent2 is in toolbox instead of motobox to avoid GPL issues
+
+TOOLS += sendevent2
+
+# Motorola - END - IKAPP-606 - wqnt78 - 3/10/2010 - apython enabler
+
 
 LOCAL_SRC_FILES := \
     start_stop.cpp \
