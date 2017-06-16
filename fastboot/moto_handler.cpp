8648e99b5f49b4aeb700c4642780e8bda9a7fb63
Donghua Yan <a18317@motorola.com>
2017-06-16 12:30:15 -0500

    fastboot: Support oem dump command
    Based on previous patch for supporting oem pull/push, another oem
    command "fastboot oem dump partition [size] [offset]" is added to
    dump the partition content to local host. This command shares the
    same interface as command "oem pull".
    
    Change-Id: Ie1d637f4a47b2770e4072654e6640b38b939bf48
    Reviewed-on: https://gerrit.mot.com/1013027
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qing Chang <qing@motorola.com>
    Submit-Approved: Jira Key

---
+++ fastboot/moto_handler.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2012-2017 Motorola Mobility LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <vector>
+
+#include "fastboot.h"
+
+static std::string next_arg(std::vector<std::string>* args) {
+    if (args->empty()) die("expected argument");
+    std::string result = args->front();
+    args->erase(args->begin());
+    return result;
+}
+
+int oem_dump_handler(const std::string& cmd, std::vector<std::string>* args)
+{
+    if (args->empty()) die("empty oem command");
+    if (args->size() < 2) die("Invalid command: fastboot oem dump <partition> [size] [offset]");
+
+    std::string command(cmd);
+    std::string partition = args->at(1);
+    while (!args->empty()) {
+        command += " " + next_arg(args);
+    }
+
+    fb_queue_command(command.c_str(), "Sending command");
+    fb_queue_dump(partition);
+
+    return 0;
+}
