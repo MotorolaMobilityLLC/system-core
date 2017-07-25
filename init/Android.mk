e7c8f591af60325dcbf539e8fa02098ba4b494c3
Pratik Kamdar <djc637@motorola.com>
2017-07-25 15:23:43 -0500

    init: Port (CR) to O
    - Port extended firmware loading logic to O
    - Also ported:
    - https://gerrit.mot.com/#/c/786003
    - http://gerrit.mot.com/786004
    
    Change-Id: I241f63a5b98a9db7a3a66f4288769e0cc540a700
    Signed-off-by: Pratik Kamdar <djc637@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1033787
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Coverity Analysis <coverity@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- init/Android.mk
+++ init/Android.mk
@@ -35,6 +35,18 @@ endif
 
 init_options += -DLOG_UEVENTS=0
 
+ifeq ($(strip $(TARGET_USE_MOT_NEW_COM)),true)
+init_options += -DMOTO_NEW_CHARGE_ONLY_MODE
+endif
+
+ifeq ($(BOARD_HAS_AUDIO_DSP_XMCS),true)
+init_options += -DMOTO_AOV_WITH_XMCS
+endif
+
+ifeq ($(BOARD_HAS_GREYBUS_INTERFACE),true)
+init_options += -DMOTO_GREYBUS_FIRMWARE
+endif
+
 init_cflags += \
     $(init_options) \
     -Wall -Wextra \
