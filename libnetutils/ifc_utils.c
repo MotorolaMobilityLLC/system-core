0c39365e6b1b919e2e3eb9c5574b49cde3e2db52
Shangbing Hu <shangbing.hu@mediatek.com>
2017-08-15 15:08:35 +0800

    [ALPS03199244] ccmni:android O migration
    [Detail]merge uplink traffic data shaping feature and IRAT feature.
    
    [Solution]merge ifc_ccmni_md_cfg & ifc_set_txq_state function.
    
    MTK-Commit-Id: bcdbd1828a40b9e33cf89c20120ee0e0cdfc262a
    
    Change-Id: I9f77230e331b420b0047a09a96d13e0f846b513c
    CR-Id:ALPS03199244
    Feature:IRAT - C2K
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>

--- libnetutils/ifc_utils.c
+++ libnetutils/ifc_utils.c
@@ -725,3 +725,129 @@ ifc_configure(const char *ifname,
 
     return 0;
 }
+#define SIOCSTXQSTATE (SIOCDEVPRIVATE + 0)  //start/stop ccmni tx queue
+#define SIOCSCCMNICFG (SIOCDEVPRIVATE + 1)  //configure ccmni/md remapping
+
+int ifc_set_txq_state(const char *ifname, int state)
+{
+    struct ifreq ifr;
+    int ret, ctl_sock;
+
+    memset(&ifr, 0, sizeof(struct ifreq));
+    strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
+    ifr.ifr_name[IFNAMSIZ - 1] = 0;
+    ifr.ifr_ifru.ifru_ivalue = state;
+
+    ctl_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if(ctl_sock < 0){
+    ALOGE("create ctl socket failed\n");
+      return -1;
+    }
+    ret = ioctl(ctl_sock, SIOCSTXQSTATE, &ifr);
+    if(ret < 0)
+       ALOGE("ifc_set_txq_state failed, err:%d(%s)\n", errno, strerror(errno));
+    else
+       ALOGI("ifc_set_txq_state as %d, ret: %d\n", state, ret);
+
+    close(ctl_sock);
+
+    return ret;
+}
+
+int ifc_ccmni_md_cfg(const char *ifname, int md_id)
+{
+    struct ifreq ifr;
+    int ret = 0;
+    int ctl_sock = 0;
+
+    ifc_init_ifr(ifname, &ifr);
+    ifr.ifr_ifru.ifru_ivalue = md_id;
+
+    ctl_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if(ctl_sock < 0){
+        printerr("ifc_ccmni_md_cfg: create ctl socket failed\n");
+        return -1;
+    }
+
+    if(ioctl(ctl_sock, SIOCSCCMNICFG, &ifr) < 0) {
+        printerr("ifc_ccmni_md_configure(ifname=%s, md_id=%d) error:%d(%s)", \
+             ifname, md_id, errno, strerror(errno));
+       ret = -1;
+    } else {
+        printerr("ifc_ccmni_md_configure(ifname=%s, md_id=%d) OK", ifname, md_id);
+    }
+
+    close(ctl_sock);
+    return ret;
+}
+
+static int setEnableIPv6(char* ifname, int on) {
+    char *path;
+    const char *value = on ? "0" : "1";
+    unsigned int size = strlen(value);
+
+    //full path: proc/sys/net/ipv6/conf/ifname/disalbe_ipv6
+    asprintf(&path, "%s/%s/%s", ipv6_proc_path, ifname, "disable_ipv6");
+    ALOGE("setEnableIPv6: set path %s to %s", path, value);
+    int fd = open(path, O_WRONLY);
+    if (fd < 0) {
+        ALOGE("Failed to open %s: %s", path, strerror(errno));
+        return -1;
+    }
+
+    if (write(fd, value, size) != size) {
+        ALOGE("Failed to write %s: %s", path, strerror(errno));
+        close(fd);
+        return -1;
+    }
+    close(fd);
+    free(path);
+    return 0;
+}
+
+static int setIPv6DefaultRoute(char* ifname, int on) {
+    char *path;
+    const char *value = on ? "1" : "0";
+    unsigned int size = strlen(value);
+
+    //full path: /proc/sys/net/ipv6/conf/ccmni0/accept_ra_defrtr
+    asprintf(&path, "%s/%s/%s", ipv6_proc_path, ifname, "accept_ra_defrtr");
+    ALOGE("setIPv6DefaultRoute: set path %s to %s", path, value);
+    int fd = open(path, O_WRONLY);
+    if (fd < 0) {
+        ALOGE("Failed to open %s: %s", path, strerror(errno));
+        return -1;
+    }
+
+    if (write(fd, value, size) != size) {
+        ALOGE("Failed to write %s: %s", path, strerror(errno));
+        close(fd);
+        return -1;
+    }
+    close(fd);
+    free(path);
+    return 0;
+}
+
+
+
+
+int ifc_ipv6_trigger_rs(char *ifname){
+    int errNo = setEnableIPv6(ifname, 0);
+     if (errNo < 0) {
+         ALOGE("ifc_ipv6_irat_triger_rs disable interface %s IPv6 fail %d",ifname, errNo);
+     }
+     errNo = setEnableIPv6(ifname, 1);
+     if (errNo < 0) {
+         ALOGE("ifc_ipv6_irat_triger_rs enalbe interface %s IPv6 fail %d",ifname, errNo);
+     }
+
+    //set accept_ra_defrtr 1
+    errNo = setIPv6DefaultRoute(ifname,1);
+    if (errNo < 0) {
+         ALOGE("ifc_ipv6_irat_triger_rs enalbe interface %s accept_ra_defrtr fail %d",ifname, errNo);
+     }
+
+    return 0;
+}
+
