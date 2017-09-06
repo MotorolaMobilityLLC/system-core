00356b01c0d78c993902b96afe78a66d892090de
Daniel Rosenberg <drosen@google.com>
2017-09-06 15:22:54 -0700

    Add derive_gid flag for mounting sdcardfs
    Turns on the derive_gid feature for sdcardfs. This was moved
    under a mount flag in the kernel.
    
    Test: If the derive_gid flag is supported, the first mount
          should succeed. If the flag is not, the second should
          succeed.
    Bug: 63245673
    Change-Id: If1c1bce13d14120732e420252cb5605d33ce7c40
    Reviewed-on: https://gerrit.mot.com/1083929
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- sdcard/sdcard.cpp
+++ sdcard/sdcard.cpp
@@ -460,7 +460,8 @@ static int usage() {
                << "    -U: specify user ID that owns device"
                << "    -m: source_path is multi-user"
                << "    -w: runtime write mount has full write access"
-               << "    -P  preserve owners on the lower file system";
+               << "    -P  preserve owners on the lower file system"
+               << "    -G: preserve owners on the lower file system";
     return 1;
 }
 
