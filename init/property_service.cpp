7cf0626f5e764946d9d7173aa6691c899bbf7da5
jixj <jixj@lenovo.com>
2017-09-11 17:02:39 +0800

    Revert "Update persist.sys.usb.config on runtime"
    This reverts commit 03bcaec5a0323cefb91c3b4631a1e464bf595579.
    
    Mot-CRs-fixed: (CR)
    
    AOSP will firstly set usb config to adb on boot. So on factory mode,
    the usb config will enumerate from adb to usbnet, this may cause
    "wait for interface" recipe failed.
    We will reset persist.sys.usb.config on init.mmi.usb.sh
    
    Change-Id: I2e3fc7724d0c2fc683aacc01a2196b5779e47763
    Reviewed-on: https://gerrit.mot.com/1056662
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Reviewed-by: Coverity Analysis <coverity@motorola.com>
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -662,22 +662,6 @@ static bool load_properties_from_file(const char* filename, const char* filter) 
     return true;
 }
 
-// persist.sys.usb.config values can't be combined on build-time when property
-// files are split into each partition.
-// So we need to apply the same rule of build/make/tools/post_process_props.py
-// on runtime.
-static void update_sys_usb_config() {
-    bool is_debuggable = android::base::GetBoolProperty("ro.debuggable", false);
-    std::string config = android::base::GetProperty("persist.sys.usb.config", "");
-    if (config.empty()) {
-        property_set("persist.sys.usb.config", is_debuggable ? "adb" : "none");
-    } else if (is_debuggable && config.find("adb") == std::string::npos &&
-               config.length() + 4 < PROP_VALUE_MAX) {
-        config.append(",adb");
-        property_set("persist.sys.usb.config", config);
-    }
-}
-
 void property_load_boot_defaults() {
     if (!load_properties_from_file("/system/etc/prop.default", NULL)) {
         // Try recovery path
@@ -689,8 +673,6 @@ void property_load_boot_defaults() {
     load_properties_from_file("/product/build.prop", NULL);
     load_properties_from_file("/odm/default.prop", NULL);
     load_properties_from_file("/vendor/default.prop", NULL);
-
-    update_sys_usb_config();
 }
 
 static void load_override_properties() {
