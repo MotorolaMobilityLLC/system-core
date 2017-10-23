56efb946ee5e117d483f2591e191d2b084ef62dd
jixj <jixj@lenovo.com>
2017-10-23 11:07:28 +0800

    Revert "IKSWL-619 init: Allow target-specific platform device base to be specified."
    Mot-CRs-fixed: IKSWO-12263
    
    This commit is cherry-pick from QCOM old branch, There is no this commit on
    QCOM lately O branch. Because of defining TARGET_PLATFORM_DEVICE_BASE
    with "/device/soc" on Boardconfig.mk,it will create "/dev/block/platform/xxx"
    and we configure dts-fstab with "/dev/block/platform/xxx".but AOSP don't have
    this commit,GSI will creae "/dev/block/platform/soc/xxx".when flash GSI and
    eraly mount,init don't find "/dev/block/platform/xxx" to cause mount failed.
    So we revert it to follow AOSP design.
    
    This reverts commit 48dae4c77f1a8d952d1fb4ed2a1ddde3431162e7.
    
    Change-Id: I1fd336764d0e8df6db0f2f467f6f80a288ddbbac
    Reviewed-on: https://gerrit.mot.com/1077083
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Coverity Analysis <coverity@motorola.com>
    Reviewed-by: Yiwen Liu <liuyw2@motorola.com>
    Reviewed-by: Qing Chang <qing@motorola.com>
    Submit-Approved: Jira Key

--- init/Android.mk
+++ init/Android.mk
@@ -150,10 +150,6 @@ LOCAL_POST_INSTALL_CMD := $(hide) mkdir -p $(TARGET_ROOT_OUT)/sbin; \
     ln -sf ../init $(TARGET_ROOT_OUT)/sbin/ueventd; \
     ln -sf ../init $(TARGET_ROOT_OUT)/sbin/watchdogd
 
-ifneq ($(strip $(TARGET_PLATFORM_DEVICE_BASE)),)
-LOCAL_CFLAGS += -D_PLATFORM_BASE="\"$(TARGET_PLATFORM_DEVICE_BASE)\""
-endif
-
 LOCAL_SANITIZE := integer
 LOCAL_CLANG := true
 include $(BUILD_EXECUTABLE)
