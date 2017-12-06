d3a7487602fe399fac57b78c0b8e568ae37008c4
kalathik <kalathik@motorola.com>
2017-12-06 19:32:36 +0530

    AID_READPROC macro value in sync with AOSP
    Since AID_QCOM_DIAG is removed, the AID value 3009 is
    unused, were this value is used in AOSP for AID_READPROC.
    
    CTS CtsBionicTestCases will check for the existance of
    the AID 3009, Otherwise this CTS testcase will fail.
    
    To sync with AOSP AID_READPROC value is changed to 3009.
    
    Change-Id: I2c5cc549dbec22bdabb28668f8f3045586e0867b
    Reviewed-on: https://gerrit.mot.com/1100349
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Srihari Sathyanarayana <sriharis@motorola.com>
    Reviewed-by: Vijayakumar Gn <vijaygn@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Reviewed-by: Thiago Kunsch Rocha <tkunsch@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: zhaoxp3 <zhaoxp3@motorola.com>

--- libcutils/include/private/android_filesystem_config.h
+++ libcutils/include/private/android_filesystem_config.h
@@ -162,7 +162,7 @@
 
 /* AID_READPROC & AID_WAKELOCK needed by system_server
  * Please keep sync with ZygoteInit.java --setgroups */
-#define AID_READPROC 3014     /* Allow /proc read access */
+#define AID_READPROC 3009     /* Allow /proc read access */
 #define AID_WAKELOCK 3015     /* Allow system wakelock read/write access */
 #define AID_UHID 3016         /* Allow read/write to /dev/uhid node */
 
