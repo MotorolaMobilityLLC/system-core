d16d13b2a7b5f233cc488a22630d0ab241d33c2e
yunma <yunma@motorola.com>
2017-12-13 23:49:19 -0600

    resolve rebase issue on O MR1
    Change-Id: I48bf09cf9411bf8c0de8d259ada428b20137aeeb

--- init/hw_tags.h
+++ init/hw_tags.h
@@ -3,7 +3,12 @@
 
 #include "property_service.h"
 
+namespace android {
+namespace init {
+
 int process_hw_mappings(const char *xml_name);
 void verify_carrier_compatibility(void);
 
+} //namesapce init
+} //namespace android
 #endif
