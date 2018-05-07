7761b04d99999665eb0a125630328ca3fb6ff49c
Po Hu <po.hu@mediatek.com>
2018-05-07 15:44:18 +0800

    [ALPS03800946] Change to use mtkPlugin namespace to get options
    [Detail]
    use VendorConfig("mtkPlugin") instead of mediatek.GetFeature
    
    Change-Id: I8cd3b20ee5eee4b5ca032eafea88e5648e3b6e3e
    CR-Id: ALPS03800946
    Feature: Mobile Log Tool

--- liblog/liblog.go
+++ liblog/liblog.go
@@ -1,10 +1,10 @@
 package mtkLogEnhance
+
 import (
 	"os"
-	"path/filepath"
+
 	"android/soong/android"
 	"android/soong/cc"
-	"android/soong/android/mediatek"
 )
 
 func mtkLogEnhanceDefaults(ctx android.LoadHookContext) {
@@ -16,24 +16,25 @@ func mtkLogEnhanceDefaults(ctx android.LoadHookContext) {
 		}
 	}
 	p := &props{}
+	vars := ctx.Config().VendorConfig("mtkPlugin")
 
-	if mtkFoFalse("MTK_LOGD_ENHANCE_DISABLE") {
+	if !vars.Bool("MTK_LOGD_ENHANCE_DISABLE") {
 		p.Target.Android.Cflags = append(p.Target.Android.Cflags, "-DMTK_LOGD_ENHANCE")
-		if mtkFoFalse("MTK_LOGDW_SOCK_BLOCK_DISABLE") {
+		if !vars.Bool("MTK_LOGDW_SOCK_BLOCK_DISABLE") {
 			p.Target.Android.Cflags = append(p.Target.Android.Cflags, "-DMTK_LOGDW_SOCK_BLOCK")
 		}
-		if mtkFoFalse("MTK_LOGD_FILTER_DISABLE") {
+		if !vars.Bool("MTK_LOGD_FILTER_DISABLE") {
 			p.Target.Android.Cflags = append(p.Target.Android.Cflags, "-DMTK_LOGD_FILTER")
 		}
-		mtkLogEnable := filepath.Join(".", "vendor/mediatek/internal/mtklog_enable")
-		if _, err := os.Stat(mtkLogEnable); err == nil {
-			if mediatek.GetFeature("MTK_ANDROID_LOG_MUCH_COUNT") != "" {
+		if _, err := os.Stat("vendor/mediatek/internal/mtklog_enable"); err == nil {
+			if vars.String("MTK_ANDROID_LOG_MUCH_COUNT") != "" {
 				p.Target.Android.Cflags = append(p.Target.Android.Cflags, "-DANDROID_LOG_MUCH_COUNT")
 			}
 		}
 	}
 	ctx.AppendProperties(p)
 }
+
 func init() {
 	android.RegisterModuleType("mtk_log_enhance_defaults", mtkLogEnhanceDefaultsFactory)
 }
@@ -43,12 +44,3 @@ func mtkLogEnhanceDefaultsFactory() android.Module {
 	android.AddLoadHook(module, mtkLogEnhanceDefaults)
 	return module
 }
-
-func mtkFoTure(key string) bool {
-	return mediatek.GetFeature(key) == "yes"
-}
-
-func mtkFoFalse(key string) bool {
-	return mediatek.GetFeature(key) != "yes"
-}
-
