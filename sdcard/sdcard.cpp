ffbdfafe862c5af51c423ca05b26ab9c65a20c3c
Shiyong Li <a22381@motorola.com>
2018-06-18 10:37:29 -0700

    Revert "(CR): Add derive_gid flag for mounting sdcardfs"
    This reverts commit 66ae400e7a92a7b89c450eed42a77f9749d396e8.
    
    The AOSP commit that 66ae400e7a92a7b89c450eed42a77f9749d396e8 intends to
    add has been on P already, we don't have to duplicate doing it.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I2d1c9f8a42cc011ac000916da895c97e03a6e201
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1195254
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: zhaoxp3 <zhaoxp3@motorola.com>

--- sdcard/sdcard.cpp
+++ sdcard/sdcard.cpp
@@ -209,8 +209,7 @@ static int usage() {
                << "    -U: specify user ID that owns device"
                << "    -m: source_path is multi-user"
                << "    -w: runtime write mount has full write access"
-               << "    -P  preserve owners on the lower file system"
-               << "    -G: preserve owners on the lower file system";
+               << "    -P  preserve owners on the lower file system";
     return 1;
 }
 
