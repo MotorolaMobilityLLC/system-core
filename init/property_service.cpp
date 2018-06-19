366882c56bb2bbfe944c720550ec34eff1bf1ed4
jixj <jixj@lenovo.com>
2018-06-19 15:41:32 +0800

    Init: Don't set persist.sys.usb.config on boot
    This fixup the following commits for P.
    a92e73b (CR): init: rename persist.sys.usb.config on boot
    f86b44f (CR): system/core bring up on P
    
    On P, All perisit properties are stored in one single file no longer
    one property one file. We can't directly rename this file
    
    Change-Id: Ibb428155c73b39b89857ab6c353bf5a847deb81b
    Reviewed-on: https://gerrit.mot.com/1195686
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: zhaoxp3 <zhaoxp3@motorola.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -682,11 +682,15 @@ static void load_override_properties() {
 }
 
 /* BEGIN IKJB42MAIN-6952, 03/13/2013, w60013, rename persist.sys.usb.config */
-void update_persistent_usb_property(void)
+bool update_persistent_usb_property(const std::string& name, const std::string& value)
 {
-        std::string persist_usb_config = android::base::GetProperty("persist.sys.usb.config", "");
-        if (!persist_usb_config.empty()) {
-            property_set("persist.mot.usb.config", persist_usb_config);
+        std::string persist_usb_config = "persist.sys.usb.config";
+        if (name == persist_usb_config) {
+            property_set("persist.mot.usb.config", value);
+            property_set("persist.sys.usb.config", "");
+            return true;
+        } else {
+            return false;
         }
 }
 /* END IKJB42MAIN-6952 */
@@ -722,13 +726,12 @@ void load_persist_props(void) {
 
     load_override_properties();
 
-    /* IKVPREL1L-4680 - update usb properties after decryption */
-    update_persistent_usb_property();
-
     /* Read persistent properties after all default values have been loaded. */
     auto persistent_properties = LoadPersistentProperties();
     for (const auto& persistent_property_record : persistent_properties.properties()) {
-        property_set(persistent_property_record.name(), persistent_property_record.value());
+        /* IKVPREL1L-4680 - update usb properties after decryption */
+        if(!update_persistent_usb_property(persistent_property_record.name(), persistent_property_record.value()))
+            property_set(persistent_property_record.name(), persistent_property_record.value());
     }
     persistent_properties_loaded = true;
     property_set("ro.persistent_properties.ready", "true");
