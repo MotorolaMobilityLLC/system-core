54d63b7ebbe7ee3df92c5c3f63c803a742e36203
Junpeng Wan <wanjp4@lenovo.com>
2018-11-13 21:09:54 +0800

    remove the aid MOT_TOMBSTONE
    Remove the unused MOT_TOMBSTONE aid Defenderinition
    for Android P aid compliance
    
    Change-Id: Ia157900665a527a46c749d36830bab12551ae970
    Signed-off-by: Junpeng Wan <wanjp4@mt.com>
    Reviewed-on: https://gerrit.mot.com/1268391
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key

--- libcutils/include/private/android_filesystem_config.h
+++ libcutils/include/private/android_filesystem_config.h
@@ -163,7 +163,6 @@
 
 /* The range 9000-9030 is reserved for moto legacy aid */
 #define AID_OEM_RESERVED_9_START 9000
-#define AID_MOT_TOMBSTONE 9006
 #define AID_OEM_RESERVED_9_END 9030
 
 #define AID_EVERYBODY 9997 /* shared between all apps in the same profile */
