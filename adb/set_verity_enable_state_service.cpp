2e121e8246c1549ea065f335c54438607cb79593
gezz1 <gezz1@lenovo.com>
2019-04-28 15:47:44 +0800

    Allow moto build C to disable verity
    Skip the '__android_log_is_debuggable' check on Moto build C
    Refer to (CR), (CR)
    
    Change-Id: I421f397c58a5afbd30df636d0c6c07aa3fb3b18f
    Reviewed-on: https://gerrit.mot.com/1345115
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- adb/set_verity_enable_state_service.cpp
+++ adb/set_verity_enable_state_service.cpp
@@ -151,18 +151,22 @@ void set_verity_enabled_state_service(int fd, void* cookie) {
                        enable ? "enable" : "disable");
         }
 
+#ifndef MOTO_BLD_C
         if (!android::base::GetBoolProperty("ro.secure", false)) {
             WriteFdFmt(fd, "verity not enabled - ENG build\n");
             return;
         }
+#endif
     }
 
     // Should never be possible to disable dm-verity on a USER build
     // regardless of using AVB or VB1.0.
+#ifndef MOTO_BLD_C
     if (!__android_log_is_debuggable()) {
         WriteFdFmt(fd, "verity cannot be disabled/enabled - USER build\n");
         return;
     }
+#endif
 
     if (using_avb) {
         // Yep, the system is using AVB.
