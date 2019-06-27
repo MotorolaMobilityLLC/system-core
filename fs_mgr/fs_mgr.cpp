b7c428c465b83c855ce2743de5e71c53a46aadfa
Russ W. Knize <Russ.Knize@motorola.com>
2019-06-27 16:13:49 +0530

    fs_mgr: retry mounting non-formattable partitions
    Ported from the CRs (CR) & (CR)
    
    If a non-formattable partition fails to mount, give them another
    chance.  Also improve some of the diagnostics and logGing when waiting
    for device nodes to appear.
    
    This CR (CR), resolves the bug in this logic
    
    When fail to mount the device, moto will retry twice to mount and
    set i to the first start index (i = top_idx -1), if mount failed
    parition is the first entry on the fstab and top_idx is 0 and i
    will become very large and for statement will break, fs_mg_mount_all
    will return encrypted code (FS_MGR_MNTALL_DEV_FILE_ENCRYPTED)
    
    Change-Id: I7f104dc781a9e2bab67b924ff0b195ac6b761cd6
    Signed-off-by: Kanagarathina Kalathi <kalathik@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1568350
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -116,6 +116,8 @@ using namespace android::fs_mgr;
 
 using namespace std::literals;
 
+#define MAX_MOUNT_RETRIES 2
+
 // record fs stat
 enum FsStatFlags {
     FS_STAT_IS_EXT4 = 0x0001,
@@ -1497,6 +1499,7 @@ int fs_mgr_mount_all(Fstab* fstab, int mount_mode)
     int error_count = 0;
     CheckpointManager checkpoint_manager;
     AvbUniquePtr avb_handle(nullptr);
+    int retry = MAX_MOUNT_RETRIES;
 
     if (fstab->empty()) {
         return FS_MGR_MNTALL_FAIL;
@@ -1638,7 +1641,11 @@ int fs_mgr_mount_all(Fstab* fstab, int mount_mode)
                 }
             }
 
-            // Success!  Go get the next one.
+            /* Success!  Go get the next one */
+            PINFO << "Successfully mounted "
+                  << attempted_entry.mount_point << " with file system type '"
+                  << attempted_entry.fs_type << ".";
+            retry = MAX_MOUNT_RETRIES;
             continue;
         }
 
@@ -1670,7 +1677,8 @@ int fs_mgr_mount_all(Fstab* fstab, int mount_mode)
             }
             if (fs_mgr_do_format(current_entry, crypt_footer) == 0) {
                 // Let's replay the mount actions.
-                i = top_idx - 1;
+                if (top_idx > 0)
+                    i = top_idx - 1;
                 continue;
             } else {
                 LERROR << __FUNCTION__ << "(): Format failed. "
@@ -1701,6 +1709,12 @@ int fs_mgr_mount_all(Fstab* fstab, int mount_mode)
                     continue;
                 }
             } else {
+		if ((top_idx > 0) && (--retry > 0)) {
+                    LERROR << "Failed to mount "
+                           << attempted_entry.mount_point << "; retrying...";
+                    i = top_idx - 1;
+                    continue;
+                }
                 PERROR << "Failed to mount an encryptable partition on"
                        << attempted_entry.blk_device << " at "
                        << attempted_entry.mount_point << " options: "
@@ -1722,6 +1736,12 @@ int fs_mgr_mount_all(Fstab* fstab, int mount_mode)
         } else {
             // fs_options might be null so we cannot use PERROR << directly.
             // Use StringPrintf to output "(null)" instead.
+            if ((top_idx > 0) && (--retry > 0)) {
+                PERROR << "Failed to mount " << attempted_entry.mount_point
+                       << "; retrying...";
+                i = top_idx - 1;
+                continue;
+            }
             if (attempted_entry.fs_mgr_flags.no_fail) {
                 PERROR << android::base::StringPrintf(
                         "Ignoring failure to mount an un-encryptable or wiped "
