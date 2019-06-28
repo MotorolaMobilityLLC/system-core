20dfff4695c6e2e2ba73be534cc0f4e8f0cf6ec5
Jins Varghese <dcw476@motorola.com>
2019-06-28 11:43:19 +0530

    Port extended firmware loading logic to Q
    Ported from (CR)
    
    Also ported https://gerrit.mot.com/#/c/1238886/ to Q
    
    Change-Id: I1f9258714cb3336dd594c149c417450ad1a181da
    Reviewed-on: https://gerrit.mot.com/1382410
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Seungyoon Lee <yoon@motorola.com>
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- init/firmware_handler.cpp
+++ init/firmware_handler.cpp
@@ -22,22 +22,26 @@
 #include <unistd.h>
 
 #include <thread>
+#include <string>
 
 #include <android-base/chrono_utils.h>
 #include <android-base/file.h>
 #include <android-base/logging.h>
 #include <android-base/unique_fd.h>
+#include <android-base/stringprintf.h>
+#include <android-base/strings.h>
 
 using android::base::Timer;
 using android::base::unique_fd;
 using android::base::WriteFully;
+using android::base::StringPrintf;
 
 namespace android {
 namespace init {
 
 static void LoadFirmware(const Uevent& uevent, const std::string& root, int fw_fd, size_t fw_size,
                          int loading_fd, int data_fd) {
-    // Start transfer.
+   // Start transfer.
     WriteFully(loading_fd, "1", 1);
 
     // Copy the firmware.
@@ -55,6 +59,96 @@ static void LoadFirmware(const Uevent& uevent, const std::string& root, int fw_f
 static bool IsBooting() {
     return access("/dev/.booting", F_OK) == 0;
 }
+
+/*
+    Special firmware look-up functionality intended for non-system media firmware (ie.
+    downloaded firmware). The folders provided must be protected via SELinux policy.
+ */
+ struct extended_fw_path {
+    const char *fw_substring;
+    const char *fw_path;
+ };
+
+ const struct extended_fw_path extended_paths[] = {
+ #ifdef MOTO_AOV_WITH_XMCS
+     {
+         .fw_substring = "-aov-",
+         .fw_path = "/data/adspd",
+     },
+     {
+         .fw_substring = "-ultrasound",
+         .fw_path = "/data/adspd",
+     },
+ #endif
+ #ifdef MOTO_GREYBUS
+     {
+         .fw_substring = "upd-",
+         .fw_path = "/data/gbfirmware",
+     },
+ #endif
+ };
+
+ static int is_hard_link(const char *path)
+ {
+     int rv = 1;
+     struct stat sb;
+
+     if(stat(path, &sb) == 0) {
+         if((S_ISDIR(sb.st_mode)) || (sb.st_nlink == 1))
+             rv = 0;
+         else
+             LOG(ERROR) << "Invalid hard link (" << path << "), nlink=" << sb.st_nlink << " ignoring!";
+     } else if (errno == ENOENT)
+         rv = 0;
+     return(rv);
+ }
+
+ static int LoadOneExtended(const Uevent& uevent, int loading_fd, int data_fd, size_t index)
+ {
+     int ret = 0;
+     std::string root = StringPrintf("/sys%s", uevent.path.c_str());
+    /* look for naming convention for the target firmware */
+     if (strstr(uevent.firmware.c_str(), extended_paths[index].fw_substring) == NULL) {
+         return 0;
+     }
+
+    std::string file = StringPrintf("%s/%s", extended_paths[index].fw_path, uevent.firmware.c_str());
+     if (is_hard_link(file.c_str())) {
+         return 0;
+     }
+
+     /* Do not consider the case /data folder is still encrypted. It is assumed
+        userspace apps needing these files are started only after data partition
+        is decrypted. */
+     unique_fd fw_fd(open(file.c_str(), O_RDONLY|O_NOFOLLOW));
+     struct stat sb;
+     if (fw_fd != -1 && fstat(fw_fd, &sb) != -1) {
+         LoadFirmware(uevent, root, fw_fd, sb.st_size, loading_fd, data_fd);
+         ret = -1;
+     } else {
+         return 0;
+     }
+     return ret;
+ }
+
+ #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
+
+ /*
+     -   The function returns the following values:
+     -   -1 - Firmware loading was either success or failure. No need to look for further folders.
+     -    0 - Firmware was not loaded. Further folders need to be looked up.
+ */
+ static int LoadFromExtended(const Uevent& uevent, int loading_fd, int data_fd)
+ {
+     size_t i;
+
+     /* Loop through all possible extended folders unless we find a firmware */
+     for (i = 0; i < ARRAY_SIZE(extended_paths); i++)
+         if (LoadOneExtended(uevent, loading_fd, data_fd, i) == -1)
+             return -1;
+
+     return 0;
+ }
 
 FirmwareHandler::FirmwareHandler(std::vector<std::string> firmware_directories)
     : firmware_directories_(std::move(firmware_directories)) {}
@@ -80,6 +174,9 @@ void FirmwareHandler::ProcessFirmwareEvent(const Uevent& uevent) {
         return;
     }
 
+    if (LoadFromExtended(uevent, loading_fd, data_fd) < 0) {
+         return;
+    }
 try_loading_again:
     for (const auto& firmware_directory : firmware_directories_) {
         std::string file = firmware_directory + uevent.firmware;
