1186156da0fc8f2d1845209467d72c82c0e1ae9e
jixj <jixj@lenovo.com>
2019-08-27 14:11:18 +0800

    fs_mgr: allow buildc to setup overlayfs when disbale verity
    when adb disable verity, adb will call fs_mgr_overlayfs_setup() but
    ALLOW_ADBD_DISABLE_VERITY is 0 on buildc, so  fs_mgr_overlayfs_setup()
    do nothing and failed to setup overlayfs.
    
    Change-Id: Ifa0ad9f67d53ee9f89a1eed70e83a177a5e20027
    Reviewed-on: https://gerrit.mot.com/1410947
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Kanagarathina Kalathi <kalathik@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1520,7 +1520,7 @@ int fs_mgr_mount_all(Fstab* fstab, int mount_mode)
         }
     }
 
-#if ALLOW_ADBD_DISABLE_VERITY == 1  // "userdebug" build
+#if (ALLOW_ADBD_DISABLE_VERITY == 1) || (defined MOTO_BLD_C)   // "userdebug" build
     fs_mgr_overlayfs_mount_all(fstab);
 #endif
 
