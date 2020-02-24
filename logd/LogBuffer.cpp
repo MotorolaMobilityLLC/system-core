86b215940cad6c0bc3a0023726866fc62c3bb9c1
xiaoy3 <xiaoy3@motorola.com>
2020-02-24 15:28:25 +0800

    Revert "(CR) logd: Fix race between flushTo and prune"
    on current logd
    isbusy, kickme and filter to control the racing
    between read and prune.
    
    Try to revert for aplogd Excessive logGing issue
    
    This reverts commit 0e7a538bf432df5ebbdef901a3761b7609839bf0.
    
    Change-Id: I986be88f6c4f9ea69dc2a077b6fe092c8b2e812f
    Reviewed-on: https://gerrit.mot.com/1502592
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -465,12 +465,6 @@ void LogBuffer::maybePrune(log_id_t id) {
     }
 }
 
-void LogBuffer::poison(
-        LogBufferElementCollection::iterator it) {
-    unsigned long *it_addr = (unsigned long *)((unsigned long)&(*it));
-    memset(it_addr, 0x6b, sizeof(unsigned long));
-}
-
 LogBufferElementCollection::iterator LogBuffer::erase(
     LogBufferElementCollection::iterator it, bool coalesce) {
     LogBufferElement* element = *it;
@@ -512,7 +506,6 @@ LogBufferElementCollection::iterator LogBuffer::erase(
                   ? element->getTag()
                   : element->getUid();
 #endif
-    poison(it);
     it = mLogElements.erase(it);
     if (doSetLast) {
         log_id_for_each(i) {
@@ -1111,7 +1104,6 @@ log_time LogBuffer::flushTo(SocketClient* reader, const log_time& start,
                             void* arg) {
     LogBufferElementCollection::iterator it;
     uid_t uid = reader->getUid();
-    unsigned long oldit_value;
 
     rdlock();
 
@@ -1148,7 +1140,6 @@ log_time LogBuffer::flushTo(SocketClient* reader, const log_time& start,
     size_t skip = maxSkip;
     for (; it != mLogElements.end(); ++it) {
         LogBufferElement* element = *it;
-        oldit_value = (unsigned long)*it;
 
         if (!--skip) {
             android::prdebug("reader.per: too many elements skipped");
@@ -1202,12 +1193,6 @@ log_time LogBuffer::flushTo(SocketClient* reader, const log_time& start,
 
         skip = maxSkip;
         rdlock();
-
-        if (oldit_value != (unsigned long)*it) {
-            unlock();
-            return curr;
-        }
-
     }
     unlock();
 
