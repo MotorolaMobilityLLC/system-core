f8629fa1ce412ff71210f6a6569a23d81a454208
Konstantin Makariev <hcv867@motorola.com>
2020-05-22 12:03:28 +0530

    init: allow model and manufacturer override
    Some benchmarks tend to report model and manufacturer names to the
    server. This exposes Motorola unreleased product info to the outside
    world and creates proprietary info "leak". Allow for model and
    manufacturer override to protect Motorola product identity.
    
    Change-Id: I3ba184ed1a986cf6f1bfd34dffa8a0aacd6121f7
    Signed-off-by: Konstantin Makariev <hcv867@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1497391
    Reviewed-by: Konstantin Makariev <kmakariev@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1573180
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1013,6 +1013,8 @@ static void ExportKernelBootProps() {
         { "ro.boot.hardware",   "ro.hardware",   "unknown", },
         { "ro.boot.revision",   "ro.revision",   "0", },
         { "ro.boot.kb_pad_port_b", "ro.com.google.ime.kb_pad_port_b", UNSET, },
+        { "ro.boot.model", "ro.product.model", UNSET, },
+        { "ro.boot.manufacturer", "ro.product.manufacturer", UNSET, },
             // clang-format on
     };
     for (const auto& prop : prop_map) {
