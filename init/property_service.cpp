63136bdb8c9a3023eeb6dbc6ffc1e61ef763e6dc
wangjiaqing <jiaqing.wang@chino-e.com>
2020-06-02 09:46:48 +0800

    EKBLACKJQ-1101: Set correct hardware sku name for BJQ
    Detail description:
    Set correct hardware sku name for BJQ.
    
    Change-Id: I28c78e7fcf9dd77983c0309287275a83654377f3
    St-result: pass

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1082,8 +1082,8 @@ void set_properties_from_hwinfo() {
     int len = strlen("band_id=");
     std::string prop_sku_value = android::base::GetProperty("ro.build.name", "");
     if (prop_sku_value == "lenovo") {
-        property_set("ro.boot.hardware.sku","XT2053-3");
-        property_set("ro.vendor.hardware.sku","XT2053-3");
+        property_set("ro.boot.hardware.sku","XT2055-3");
+        property_set("ro.vendor.hardware.sku","XT2055-3");
     } else if (ReadFileToString(cmdline_path, &file_content)) {
         file_band = file_content.substr(len,8);
         property_set("ro.boot.hardware.sku",file_band);
