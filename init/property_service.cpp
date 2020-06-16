bc1b60fca8df76cdf1f528a0905eb6f041143e9f
kalathik <kalathik@motorola.com>
2020-06-16 18:26:22 +0530

    init: Map ro.boot.carrier to ro.carrier
    This will get the boot time property set using
    utags or bootloader config to map into ro.carrier.
    
    Testing:
    fastboot oem config carrier vzw
    fastboot oem config cmdl "androidboot.carrier=vzw"
    
    on next boot, the ro.carrier and ro.boot.carrier value is set
    to 'vzw'. Which we set through fastboot.
    
    Change-Id: I0db50d32a536dab35510f4f0b36a1c4b6d792e4a
    Signed-off-by: Kanagarathina Kalathi <kalathik@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1624194
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1853275
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2036620

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1518,6 +1518,7 @@ static void ExportKernelBootProps() {
         { "ro.boot.kb_pad_port_b", "ro.com.google.ime.kb_pad_port_b", UNSET, },
         { "ro.boot.model", "ro.product.model", UNSET, },
         { "ro.boot.manufacturer", "ro.product.manufacturer", UNSET, },
+        { "ro.boot.carrier",   "ro.carrier",   UNSET, },
             // clang-format on
     };
     for (const auto& prop : prop_map) {
