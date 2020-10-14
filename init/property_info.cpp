88bdc7e7bd684a6e60ac79d5c518aafd1c62a938
wangjiaqing <jiaqing.wang@chino-e.com>
2020-10-14 15:24:22 +0800

    EKmoto_e(7)-2494: Client id of Claro Argentina changed to same as other AR carriers
    Detail description:
    Client id of Claro Argentina changed to same as other AR carriers.
    [android-motorola]
    [android-motorola]
    [android-motorola-rvo3]
    
    St-result: PASS
    Change-Id: I01d2cd388e77123ecc2af130e5002103831b2967

--- init/property_info.cpp
+++ init/property_info.cpp
@@ -250,9 +250,8 @@ void set_system_properties(){
             if (carrier_ontim == "openmx_retmx" || carrier_ontim == "amxmx_amxmx"
              || carrier_ontim == "amxmx_amxmxsl" || carrier_ontim == "amxpe_claro"
              || carrier_ontim == "amxco_claro" || carrier_ontim == "amxbr_clarobr"
-             || carrier_ontim == "retar_retag" || carrier_ontim == "amxcl_clarocl"
-             || carrier_ontim == "amxla_amxlag" || carrier_ontim == "amxla_amxdomi"
-             || carrier_ontim == "amxbr_brmanaus") {
+             || carrier_ontim == "amxbr_brmanaus" || carrier_ontim == "amxcl_clarocl"
+             || carrier_ontim == "amxla_amxlag" || carrier_ontim == "amxla_amxdomi") {
                 property_set(prop_amclient, prop_clientcountry_value);
                 property_set(prop_msclient, prop_clientrevc_value);
             } else if (carrier_ontim == "attmx_attmx") {
@@ -319,8 +318,8 @@ void set_system_properties(){
             if (carrier_ontim == "openmx_retmx" || carrier_ontim == "amxmx_amxmx"
              || carrier_ontim == "amxmx_amxmxsl" || carrier_ontim == "amxpe_claro"
              || carrier_ontim == "amxco_claro" || carrier_ontim == "amxbr_clarobr"
-             || carrier_ontim == "retar_retag" || carrier_ontim == "amxcl_clarocl"
-             || carrier_ontim == "amxla_amxlag" || carrier_ontim == "amxla_amxdomi") {
+             || carrier_ontim == "amxcl_clarocl" || carrier_ontim == "amxla_amxlag"
+             || carrier_ontim == "amxla_amxdomi") {
                 property_set(prop_amclient, prop_clientcountry_value);
                 property_set(prop_msclient, prop_clientrevc_value);
             } else if (carrier_ontim == "attmx_attmx") {
