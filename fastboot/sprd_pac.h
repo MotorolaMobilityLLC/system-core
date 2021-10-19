ef599572716f67127cbf24605d5ab56ea28b58da
hezhigang <zhigang.he@chino-e.com>
2021-10-19 19:03:55 +0800

    Kmoto_e32-684: fastboot: check Compatible by product_name
    pac file:
    vendor/sprd/release/pac_config/ums9230_4h10.ini
      PAC_PRODUCT=ums9230_4h10
    
    fastboot var:
      product_name=ums9230_4h10
    
    Change-Id: I3d0974a7f61ff72f9a0f2786a4f633ba1c241ee1

--- fastboot/sprd_pac.h
+++ fastboot/sprd_pac.h
@@ -325,7 +325,7 @@ static bool isCompatibleProduct(std::string &pacProduct, const std::string &curP
     if (strcmp(pac_product, "ums9230_4h10_go") == 0) {
         return strcmp(cur_product, "aruba") == 0;
     }
-    return false;
+    return strcmp(pac_product, cur_product) == 0;
 }
 
 static bool readPacToImgs(const std::string& pacFilename, PacInfo& pacInfo,
