0233c48ad438229276dc7f8c48bbdd634eba52a9
Andrew Wheeler <awheeler@motorola.com>
2022-03-11 09:21:08 -0600

    init: Override system SPL with vendor SPL
    The system SPL, ro.build.version.security_patch,
    must represent the patch level of all firmware images.  For
    "vendor freeze" builds, the vendor partition patch level,
    ro.vendor.build.security_patch, may be lower than the system
    patch level.  So implement init code to override the SPL
    system property to the minimum of the system SPL and
    the vendor SPL that are set in the build.prop files.
    
    Change-Id: I99aaaa3347c207c047aadc13298e98167e528fe5
    Reviewed-on: https://gerrit.mot.com/2213988
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Rodrigo Burger <burger@motorola.com>
    Submit-Approved: Jira Key

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -807,6 +807,28 @@ static void load_override_properties() {
     }
 }
 
+// Motorola: Overrided the system SPL with the vendor SPL
+static void mmi_property_override_spl(std::map<std::string, std::string>* properties) {
+    const std::string EMPTY = "";
+    auto vendor_spl = properties->find("ro.vendor.build.security_patch");
+    if (vendor_spl != properties->end()) {
+        auto vendor_spl_val = vendor_spl->second;
+        if (vendor_spl_val != EMPTY) {
+            // vendor SPL is set, so check it vs. the system spl
+            LOG(INFO) << "vendor_spl: " << vendor_spl_val;
+            auto system_spl = properties->find("ro.build.version.security_patch");
+            if (system_spl != properties->end()) {
+                auto system_spl_val = system_spl->second;
+                LOG(INFO) << "system_spl: " << system_spl_val;
+                if (system_spl_val > vendor_spl_val) {
+                    LOG(INFO) << "overriding system SPL to: " << vendor_spl_val;
+                    system_spl->second = vendor_spl_val;
+                }
+            }
+        }
+    }
+}
+
 /* Motorola:
  * Generate the named property's suffix property, and
  * append it to the property's value.
@@ -1213,6 +1235,9 @@ void PropertyLoadBootDefaults() {
         LOG(INFO) << "Loading " << kDebugRamdiskProp;
         load_properties_from_file(kDebugRamdiskProp, nullptr, &properties);
     }
+
+    // Motorola: Override the system SPL with the vendor SPL, if vendor SPL is lower.
+    mmi_property_override_spl(&properties);
 
     for (const auto& [name, value] : properties) {
         std::string error;
